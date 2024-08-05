617a6b7d0ec10b559e5e66a728c527a9eb38d0dc
Wim Haeck <whaeck@gmail.com>
2024-08-05 16:58:04 -0600

    Adding create function for TabulatedAngularDistributions

---
+++ src/dryad/format/endf/createTabulatedAngularDistributions.hpp
@@ -0,0 +1,56 @@
+#ifndef NJOY_DRYAD_FORMAT_ENDF_TABULATEDANGULARDISTRIBUTIONS
+#define NJOY_DRYAD_FORMAT_ENDF_TABULATEDANGULARDISTRIBUTIONS
+
+// system includes
+#include <vector>
+
+// other includes
+#include "tools/Log.hpp"
+#include "dryad/id/ParticleID.hpp"
+#include "dryad/format/createVector.hpp"
+#include "dryad/format/endf/createBoundaries.hpp"
+#include "dryad/format/endf/createInterpolants.hpp"
+#include "dryad/format/endf/createTabulatedAngularDistribution.hpp"
+#include "dryad/TabulatedAngularDistributions.hpp"
+#include "ENDFtk/section/26.hpp"
+
+namespace njoy {
+namespace dryad {
+namespace format {
+namespace endf {
+
+  /**
+   *  @brief Create a TabulatedAngularDistributions instance from a parsed
+   *         ENDF MF6 DiscreteTwoBodyScattering component
+   */
+  TabulatedAngularDistributions
+  createTabulatedAngularDistributions( const ENDFtk::section::Type< 26 >::DiscreteTwoBodyScattering& distribution ) {
+
+    try {
+
+      auto energies = createVector( distribution.incidentEnergies() );
+      std::vector< TabulatedAngularDistribution > distributions;
+      distributions.reserve( energies.size() );
+      for ( auto&& table : distribution.distributions() ) {
+
+        distributions.emplace_back( createTabulatedAngularDistribution( table ) );
+      }
+      auto boundaries = createBoundaries( distribution.boundaries() );
+      auto interpolants = createInterpolants( distribution.interpolants() );
+      return TabulatedAngularDistributions(
+               std::move( energies ), std::move( distributions ),
+               std::move( boundaries ), std::move( interpolants ) );
+    }
+    catch ( ... ) {
+
+      Log::info( "Error encountered while creating an average reaction product energy table" );
+      throw;
+    }
+  }
+
+} // endf namespace
+} // format namespace
+} // dryad namespace
+} // njoy namespace
+
+#endif
