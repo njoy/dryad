d9eb3534d9226019e324f63d28826c7f8f2a313e
Wim Haeck <whaeck@gmail.com>
2024-11-08 06:04:11 -0700

    Adding energy range conversion

---
+++ src/dryad/format/gnds/convertEnergies.hpp
@@ -0,0 +1,41 @@
+#ifndef NJOY_DRYAD_FORMAT_GNDS_CONVERTENERGIES
+#define NJOY_DRYAD_FORMAT_GNDS_CONVERTENERGIES
+
+// system includes
+#include <vector>
+
+// other includes
+#include "tools/Log.hpp"
+
+namespace njoy {
+namespace dryad {
+namespace format {
+namespace gnds {
+
+  /**
+   *  @brief Convert an energy range from GNDS into eV
+   */
+  void convertEnergies( std::vector< double >& energies, const std::string& unit ) {
+
+    if ( unit != "eV" ) {
+
+      if ( unit == "MeV" ) {
+
+        std::for_each( energies.begin(), energies.end(), 
+                      [] ( double& energy ) { energy *= 1e+6; } );
+      }
+      else {
+
+        Log::error( "Cannot convert energy with unit \'{}\' to eV, "
+                    "contact dryad developers", unit );
+        throw std::exception();
+      }
+    }
+  }
+
+} // gnds namespace
+} // format namespace
+} // dryad namespace
+} // njoy namespace
+
+#endif
