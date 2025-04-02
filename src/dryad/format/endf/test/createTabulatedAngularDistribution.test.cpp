8ec36eb57dc1d8a0b78cf9be101f96a6488bb287
Wim Haeck <whaeck@gmail.com>
2025-04-02 17:51:02 -0600

    Updating tests for angular distribution normalisation

--- src/dryad/format/endf/test/createTabulatedAngularDistribution.test.cpp
+++ src/dryad/format/endf/test/createTabulatedAngularDistribution.test.cpp
@@ -38,24 +38,24 @@ SCENARIO( "createTabulatedAngularDistribution" ) {
     } // WHEN
   } // GIVEN
 
-//  GIVEN( "ENDF MF26 LAW = 2 TabulatedDistribution" ) {
-//
-//    using DiscreteTwoBodyScattering = njoy::ENDFtk::section::Type< 26 >::DiscreteTwoBodyScattering;
-//
-//    auto tape = njoy::ENDFtk::tree::fromFile( "e-001_H_000.endf" );
-//    auto section = tape.materials().front().section( 26, 525 ).parse< 26 >();
-//    auto distribution = std::get< DiscreteTwoBodyScattering >( section.reactionProduct( 11 ).distribution() );
-//
-//    WHEN( "a single parsed MF26 multiplicity is given" ) {
-//
-//      THEN( "it can be converted" ) {
-//
-//        auto chunk = format::endf::createTabulatedAngularDistribution( distribution.distributions()[0] );
-//
-//        verifyElectronChunk( chunk );
-//      } // THEN
-//    } // WHEN
-//  } // GIVEN
+  GIVEN( "ENDF MF26 LAW = 2 TabulatedDistribution" ) {
+
+    using DiscreteTwoBodyScattering = njoy::ENDFtk::section::Type< 26 >::DiscreteTwoBodyScattering;
+
+    auto tape = njoy::ENDFtk::tree::fromFile( "e-001_H_000.endf" );
+    auto section = tape.materials().front().section( 26, 525 ).parse< 26 >();
+    auto distribution = std::get< DiscreteTwoBodyScattering >( section.reactionProduct( 11 ).distribution() );
+
+    WHEN( "a single parsed MF26 multiplicity is given" ) {
+
+      THEN( "it can be converted" ) {
+
+        auto chunk = format::endf::createTabulatedAngularDistribution( distribution.distributions()[0] );
+
+        verifyElectronChunk( chunk );
+      } // THEN
+    } // WHEN
+  } // GIVEN
 } // SCENARIO
 
 void verifyNeutronChunk( const TabulatedAngularDistribution& chunk ) {
@@ -93,8 +93,8 @@ void verifyElectronChunk( const TabulatedAngularDistribution& chunk ) {
   CHECK( InterpolationType::LinearLinear == pdf.interpolants()[0] );
   CHECK_THAT( -1.      , WithinRel( pdf.cosines()[0] ) );
   CHECK_THAT(  0.999999, WithinRel( pdf.cosines()[1] ) );
-  CHECK_THAT( 0.5, WithinRel( pdf.values()[0] ) );
-  CHECK_THAT( 0.5, WithinRel( pdf.values()[1] ) );
+  CHECK_THAT( 1. / 1.999999, WithinRel( pdf.values()[0] ) );
+  CHECK_THAT( 1. / 1.999999, WithinRel( pdf.values()[1] ) );
 
   CHECK( false == chunk.hasCdf() );
   CHECK_THROWS( chunk.cdf() );
--- src/dryad/format/endf/test/test_verification_functions.hpp
+++ src/dryad/format/endf/test/test_verification_functions.hpp
@@ -327,34 +327,45 @@ void verifyElectronElasticReaction( const Reaction& elastic ) {
   CHECK( 95 == angle.distributions()[14].pdf().values().size() );
   CHECK( 96 == angle.distributions()[15].pdf().cosines().size() );
   CHECK( 96 == angle.distributions()[15].pdf().values().size() );
-  CHECK_THAT( -1.          , WithinRel( angle.distributions()[0].pdf().cosines()[0] ) );
-  CHECK_THAT(  0.999999    , WithinRel( angle.distributions()[0].pdf().cosines()[1] ) );
-  CHECK_THAT(  0.5         , WithinRel( angle.distributions()[0].pdf().values()[0] ) );
-  CHECK_THAT(  0.5         , WithinRel( angle.distributions()[0].pdf().values()[1] ) );
-  CHECK_THAT( -1.          , WithinRel( angle.distributions()[1].pdf().cosines()[0] ) );
-  CHECK_THAT( -0.93        , WithinRel( angle.distributions()[1].pdf().cosines()[1] ) );
-  CHECK_THAT(  0.9962001   , WithinRel( angle.distributions()[1].pdf().cosines()[28] ) );
-  CHECK_THAT(  0.999999    , WithinRel( angle.distributions()[1].pdf().cosines()[29] ) );
-  CHECK_THAT(  0.00293923  , WithinRel( angle.distributions()[1].pdf().values()[0] ) );
-  CHECK_THAT(  0.00314865  , WithinRel( angle.distributions()[1].pdf().values()[1] ) );
-  CHECK_THAT( 42.358       , WithinRel( angle.distributions()[1].pdf().values()[28] ) );
-  CHECK_THAT( 62.1102      , WithinRel( angle.distributions()[1].pdf().values()[29] ) );
-  CHECK_THAT( -1.          , WithinRel( angle.distributions()[14].pdf().cosines()[0] ) );
-  CHECK_THAT( -0.995       , WithinRel( angle.distributions()[14].pdf().cosines()[1] ) );
-  CHECK_THAT(  0.9999989   , WithinRel( angle.distributions()[14].pdf().cosines()[93] ) );
-  CHECK_THAT(  0.999999    , WithinRel( angle.distributions()[14].pdf().cosines()[94] ) );
-  CHECK_THAT(  1.51157e-11 , WithinRel( angle.distributions()[14].pdf().values()[0] ) );
-  CHECK_THAT(  6.43040e-10 , WithinRel( angle.distributions()[14].pdf().values()[1] ) );
-  CHECK_THAT(   8.14179e+5 , WithinRel( angle.distributions()[14].pdf().values()[93] ) );
-  CHECK_THAT(   9.84753e+5 , WithinRel( angle.distributions()[14].pdf().values()[94] ) );
-  CHECK_THAT( -1.          , WithinRel( angle.distributions()[15].pdf().cosines()[0] ) );
-  CHECK_THAT( -0.9999      , WithinRel( angle.distributions()[15].pdf().cosines()[1] ) );
-  CHECK_THAT(  0.9999989   , WithinRel( angle.distributions()[15].pdf().cosines()[94] ) );
-  CHECK_THAT(  0.999999    , WithinRel( angle.distributions()[15].pdf().cosines()[95] ) );
-  CHECK_THAT(  6.25670e-13 , WithinRel( angle.distributions()[15].pdf().values()[0] ) );
-  CHECK_THAT(  1.25808e-11 , WithinRel( angle.distributions()[15].pdf().values()[1] ) );
-  CHECK_THAT(   8.15658e+5 , WithinRel( angle.distributions()[15].pdf().values()[94] ) );
-  CHECK_THAT(   9.86945e+5 , WithinRel( angle.distributions()[15].pdf().values()[95] ) );
+
+  // dryad normalises distributions upon construction
+  // the numbers in the tests given below are the values as found in the test
+  // file so they need to be normalised. the following values are the scaling
+  // factors that need to be applied (calculated by integrating the distributions
+  // in excel).
+  double scale01 = 2. / 1.999999;
+  double scale02 = 1. / 1.00000016366000;
+  double scale14 = 1. / 1.00000048323214;
+  double scale15 = 1. / 1.00000000937718;
+
+  CHECK_THAT(           -1.         , WithinRel( angle.distributions()[0].pdf().cosines()[0] ) );
+  CHECK_THAT(            0.999999   , WithinRel( angle.distributions()[0].pdf().cosines()[1] ) );
+  CHECK_THAT( scale01 *  0.5        , WithinRel( angle.distributions()[0].pdf().values()[0] ) );
+  CHECK_THAT( scale01 *  0.5        , WithinRel( angle.distributions()[0].pdf().values()[1] ) );
+  CHECK_THAT(           -1.         , WithinRel( angle.distributions()[1].pdf().cosines()[0] ) );
+  CHECK_THAT(           -0.93       , WithinRel( angle.distributions()[1].pdf().cosines()[1] ) );
+  CHECK_THAT(            0.9962001  , WithinRel( angle.distributions()[1].pdf().cosines()[28] ) );
+  CHECK_THAT(            0.999999   , WithinRel( angle.distributions()[1].pdf().cosines()[29] ) );
+  CHECK_THAT( scale02 *  0.00293923 , WithinRel( angle.distributions()[1].pdf().values()[0] ) );
+  CHECK_THAT( scale02 *  0.00314865 , WithinRel( angle.distributions()[1].pdf().values()[1] ) );
+  CHECK_THAT( scale02 * 42.358      , WithinRel( angle.distributions()[1].pdf().values()[28] ) );
+  CHECK_THAT( scale02 * 62.1102     , WithinRel( angle.distributions()[1].pdf().values()[29] ) );
+  CHECK_THAT(           -1.         , WithinRel( angle.distributions()[14].pdf().cosines()[0] ) );
+  CHECK_THAT(           -0.995      , WithinRel( angle.distributions()[14].pdf().cosines()[1] ) );
+  CHECK_THAT(            0.9999989  , WithinRel( angle.distributions()[14].pdf().cosines()[93] ) );
+  CHECK_THAT(            0.999999   , WithinRel( angle.distributions()[14].pdf().cosines()[94] ) );
+  CHECK_THAT( scale14 *  1.51157e-11, WithinRel( angle.distributions()[14].pdf().values()[0] ) );
+  CHECK_THAT( scale14 *  6.43040e-10, WithinRel( angle.distributions()[14].pdf().values()[1] ) );
+  CHECK_THAT( scale14 *   8.14179e+5, WithinRel( angle.distributions()[14].pdf().values()[93] ) );
+  CHECK_THAT( scale14 *   9.84753e+5, WithinRel( angle.distributions()[14].pdf().values()[94] ) );
+  CHECK_THAT(           -1.         , WithinRel( angle.distributions()[15].pdf().cosines()[0] ) );
+  CHECK_THAT(           -0.9999     , WithinRel( angle.distributions()[15].pdf().cosines()[1] ) );
+  CHECK_THAT(            0.9999989  , WithinRel( angle.distributions()[15].pdf().cosines()[94] ) );
+  CHECK_THAT(            0.999999   , WithinRel( angle.distributions()[15].pdf().cosines()[95] ) );
+  CHECK_THAT( scale15 *  6.25670e-13, WithinRel( angle.distributions()[15].pdf().values()[0] ) );
+  CHECK_THAT( scale15 *  1.25808e-11, WithinRel( angle.distributions()[15].pdf().values()[1] ) );
+  CHECK_THAT( scale15 *   8.15658e+5, WithinRel( angle.distributions()[15].pdf().values()[94] ) );
+  CHECK_THAT( scale15 *   9.86945e+5, WithinRel( angle.distributions()[15].pdf().values()[95] ) );
   CHECK( false == angle.distributions()[0].hasCdf() );
   CHECK( false == angle.distributions()[1].hasCdf() );
   CHECK( false == angle.distributions()[14].hasCdf() );
