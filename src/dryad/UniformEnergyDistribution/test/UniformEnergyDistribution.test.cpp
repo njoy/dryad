19a7c30821af3a1cdbab14da893dd38612e3530d
Wim Haeck <whaeck@gmail.com>
2025-04-04 10:23:36 -0600

    Addign energy distribution test

--- src/dryad/UniformEnergyDistribution/test/UniformEnergyDistribution.test.cpp
+++ src/dryad/UniformEnergyDistribution/test/UniformEnergyDistribution.test.cpp
@@ -14,7 +14,7 @@ using namespace njoy::dryad;
 
 SCENARIO( "UniformEnergyDistribution" ) {
 
-  GIVEN( "Cosines for a discrete cosine distribution" ) {
+  GIVEN( "Cosines for a discrete energy distribution" ) {
 
     WHEN( "the data is given explicitly" ) {
 
@@ -27,25 +27,25 @@ SCENARIO( "UniformEnergyDistribution" ) {
             "be tested" ) {
 
         CHECK( UniformDistributionType::Discrete == chunk.type() );
-        CHECK( 3 == chunk.numberCosines() );
+        CHECK( 3 == chunk.numberEnergies() );
 
         CHECK_THAT(  2., WithinRel( chunk.energies()[0] ) );
         CHECK_THAT(  3., WithinRel( chunk.energies()[1] ) );
         CHECK_THAT( 10., WithinRel( chunk.energies()[2] ) );
       } // THEN
 
-      THEN( "the average cosine can be calculated" ) {
+      THEN( "the average energy can be calculated" ) {
 
         CHECK_THAT( 5., WithinRel( chunk.averageEnergy() ) );
       } // THEN
     } // WHEN
   } // GIVEN
 
-  GIVEN( "Cosines for a discrete cosine distribution" ) {
+  GIVEN( "Cosines for a discrete energy distribution" ) {
 
     WHEN( "the data is given explicitly" ) {
 
-      const std::vector< double > energies = { 0., 1., 4., 6. };
+      const std::vector< double > energies = { 0., 1., 4., 8., 10. };
       UniformDistributionType type = UniformDistributionType::Interval;
 
       UniformEnergyDistribution chunk( std::move( energies ), std::move( type ) );
@@ -54,17 +54,18 @@ SCENARIO( "UniformEnergyDistribution" ) {
             "be tested" ) {
 
         CHECK( UniformDistributionType::Interval == chunk.type() );
-        CHECK( 4 == chunk.numberCosines() );
+        CHECK( 4 == chunk.numberEnergies() );
 
-        CHECK_THAT( 0., WithinRel( chunk.energies()[0] ) );
-        CHECK_THAT( 1., WithinRel( chunk.energies()[1] ) );
-        CHECK_THAT( 4., WithinRel( chunk.energies()[2] ) );
-        CHECK_THAT( 6., WithinRel( chunk.energies()[3] ) );
+        CHECK_THAT(  0., WithinRel( chunk.energies()[0] ) );
+        CHECK_THAT(  1., WithinRel( chunk.energies()[1] ) );
+        CHECK_THAT(  4., WithinRel( chunk.energies()[2] ) );
+        CHECK_THAT(  8., WithinRel( chunk.energies()[3] ) );
+        CHECK_THAT( 10., WithinRel( chunk.energies()[4] ) );
       } // THEN
 
-      THEN( "the average cosine can be calculated" ) {
+      THEN( "the average energy can be calculated" ) {
 
-        CHECK_THAT( 2.5, WithinRel( chunk.averageEnergy() ) );
+        CHECK_THAT( 4.5, WithinRel( chunk.averageEnergy() ) );
       } // THEN
     } // WHEN
   } // GIVEN
