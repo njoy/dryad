af8b601a6a70ec98b983c0ff584e960bcfcbc793
Wim Haeck <whaeck@gmail.com>
2025-10-11 03:25:31 -0600

    updatign C++ tests for orbital momentum values

--- test/resonances/CoulombPhaseShiftDifference.test.cpp
+++ test/resonances/CoulombPhaseShiftDifference.test.cpp
@@ -21,6 +21,7 @@ SCENARIO( "CoulombPhaseShiftDifference" ) {
 
     CoulombPhaseShiftDifference w( 0 );
 
+    CHECK( 0 == w.orbitalMomentum() );
     CHECK_THAT( 0., WithinRel( w( 0.00 ) ) );
     CHECK_THAT( 0., WithinRel( w( 0.25 ) ) );
     CHECK_THAT( 0., WithinRel( w( 0.50 ) ) );
@@ -45,6 +46,7 @@ SCENARIO( "CoulombPhaseShiftDifference" ) {
 
     w = CoulombPhaseShiftDifference( 1 );
 
+    CHECK( 1 == w.orbitalMomentum() );
     CHECK_THAT( 0                , WithinRel( w( 0.00 ) ) );
     CHECK_THAT( 0.244978663126864, WithinRel( w( 0.25 ) ) );
     CHECK_THAT( 0.463647609000806, WithinRel( w( 0.50 ) ) );
@@ -69,6 +71,7 @@ SCENARIO( "CoulombPhaseShiftDifference" ) {
 
     w = CoulombPhaseShiftDifference( 2 );
 
+    CHECK( 2 == w.orbitalMomentum() );
     CHECK_THAT( 0                , WithinRel( w( 0.00 ) ) );
     CHECK_THAT( 0.369333657673626, WithinRel( w( 0.25 ) ) );
     CHECK_THAT( 0.708626272127670, WithinRel( w( 0.50 ) ) );
@@ -93,6 +96,7 @@ SCENARIO( "CoulombPhaseShiftDifference" ) {
 
     w = CoulombPhaseShiftDifference( 3 );
 
+    CHECK( 3 == w.orbitalMomentum() );
     CHECK_THAT( 0                , WithinRel( w( 0.00 ) ) );
     CHECK_THAT( 0.452474889562067, WithinRel( w( 0.25 ) ) );
     CHECK_THAT( 0.873774949542297, WithinRel( w( 0.50 ) ) );
@@ -117,6 +121,7 @@ SCENARIO( "CoulombPhaseShiftDifference" ) {
 
     w = CoulombPhaseShiftDifference( 4 );
 
+    CHECK( 4 == w.orbitalMomentum() );
     CHECK_THAT( 0                , WithinRel( w( 0.00 ) ) );
     CHECK_THAT( 0.514893699558024, WithinRel( w( 0.25 ) ) );
     CHECK_THAT( 0.998129944089059, WithinRel( w( 0.50 ) ) );
--- test/resonances/HardSpherePenetrability.test.cpp
+++ test/resonances/HardSpherePenetrability.test.cpp
@@ -18,6 +18,7 @@ SCENARIO( "HardSpherePenetrability" ) {
 
     HardSpherePenetrability penetrability( 0 );
 
+    CHECK( 0 == penetrability.orbitalMomentum() );
     CHECK_THAT( 0.00, WithinRel( penetrability( 0.00 ) ) );
     CHECK_THAT( 0.25, WithinRel( penetrability( 0.25 ) ) );
     CHECK_THAT( 0.50, WithinRel( penetrability( 0.50 ) ) );
@@ -42,6 +43,7 @@ SCENARIO( "HardSpherePenetrability" ) {
 
     penetrability = HardSpherePenetrability( 1 );
 
+    CHECK( 1 == penetrability.orbitalMomentum() );
     CHECK_THAT( 0.0                   , WithinRel( penetrability( 0.00 ) ) );
     CHECK_THAT( 0.014705882352941176  , WithinRel( penetrability( 0.25 ) ) );
     CHECK_THAT( 0.10000000000000001   , WithinRel( penetrability( 0.50 ) ) );
@@ -66,6 +68,7 @@ SCENARIO( "HardSpherePenetrability" ) {
 
     penetrability = HardSpherePenetrability( 2 );
 
+    CHECK( 2 == penetrability.orbitalMomentum() );
     CHECK_THAT( 0.0                   , WithinRel( penetrability( 0.00 ) ) );
     CHECK_THAT( 0.00010624734381640459, WithinRel( penetrability( 0.25 ) ) );
     CHECK_THAT( 0.0031847133757961785 , WithinRel( penetrability( 0.50 ) ) );
@@ -90,6 +93,7 @@ SCENARIO( "HardSpherePenetrability" ) {
 
     penetrability = HardSpherePenetrability( 3 );
 
+    CHECK( 3 == penetrability.orbitalMomentum() );
     CHECK_THAT( 0.0                   , WithinRel( penetrability( 0.00 ) ) );
     CHECK_THAT( 2.6789053349863964e-07, WithinRel( penetrability( 0.25 ) ) );
     CHECK_THAT( 3.3014196104324863e-05, WithinRel( penetrability( 0.50 ) ) );
@@ -114,6 +118,7 @@ SCENARIO( "HardSpherePenetrability" ) {
 
     penetrability = HardSpherePenetrability( 4 );
 
+    CHECK( 4 == penetrability.orbitalMomentum() );
     CHECK_THAT( 0.0                   , WithinRel( penetrability( 0.00 ) ) );
     CHECK_THAT( 3.4292596900052667e-10, WithinRel( penetrability( 0.25 ) ) );
     CHECK_THAT( 1.7091673927779473e-07, WithinRel( penetrability( 0.50 ) ) );
