f4c8a2e51c8081fefa3c863606d1a2a895df21c6
Wim Haeck <whaeck@gmail.com>
2025-10-20 21:12:21 -0600

    adding a create formalism function for endf and add a test for it

---
+++ src/dryad/format/endf/resonances/lrf7/createFormalism.hpp
@@ -0,0 +1,45 @@
+#ifndef NJOY_DRYAD_FORMAT_ENDF_RESONANCES_LRF7_CREATEFORMALISM
+#define NJOY_DRYAD_FORMAT_ENDF_RESONANCES_LRF7_CREATEFORMALISM
+
+// system includes
+#include <vector>
+
+// other includes
+#include "tools/Log.hpp"
+#include "dryad/resonances/Formalism.hpp"
+#include "ENDFtk/section/2/151.hpp"
+
+namespace njoy {
+namespace dryad {
+namespace format {
+namespace endf {
+namespace resonances {
+namespace lrf7 {
+
+  /**
+   *  @brief Create the formalism enum value
+   *
+   *  @param[in] endf   the parsed ENDF LRF7 data
+   */
+  auto createFormalism( const ENDFtk::section::Type< 2, 151 >::RMatrixLimited& endf ) {
+
+    switch ( endf.formalism() ) {
+
+      case 3 : return dryad::resonances::Formalism::ReichMoore;
+      case 4 : return dryad::resonances::Formalism::GeneralRMatrix;
+      default : {
+
+        Log::error( "LRF7 resonance formalism (KRM) type \'{}\' is currently not supported", endf.formalism() );
+        throw std::exception();
+      }
+    }
+  }
+
+} // lrf7 namespace
+} // resonances namespace
+} // endf namespace
+} // format namespace
+} // dryad namespace
+} // njoy namespace
+
+#endif
