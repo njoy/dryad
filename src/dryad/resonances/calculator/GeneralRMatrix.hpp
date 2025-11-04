044c67c9f44ea3735a1e93b6b1209740354b8ae0
Wim Haeck <whaeck@gmail.com>
2025-11-03 17:09:27 -0700

    starting work on general r-matrix

--- src/dryad/resonances/calculator/GeneralRMatrix.hpp
+++ src/dryad/resonances/calculator/GeneralRMatrix.hpp
@@ -2,8 +2,13 @@
 #define NJOY_DRYAD_RESONANCES_CALCULATOR_GENERALRMATRIX
 
 // system includes
+#include <variant>
+#include <vector>
 
 // other includes
+#include "dryad/matrix.hpp"
+#include "dryad/resonances/calculator/Constant.hpp"
+#include "dryad/resonances/calculator/ShiftFactor.hpp"
 
 namespace njoy {
 namespace dryad {
@@ -16,9 +21,29 @@ namespace calculator {
    */
   class GeneralRMatrix {
 
-    /* type aliases */
+    using LMatrix = std::variant< calculator::Constant, calculator::ShiftFactor >;
 
     /* fields */
+
+    LMatrix l_matrix_;
+
+    matrix::Matrix< std::complex< double > > t_matrix_;
+    matrix::Matrix< std::complex< double > > w_matrix_;
+    matrix::Matrix< std::complex< double > > u_matrix_;
+    matrix::DiagonalMatrix< double > sqrt_p_matrix_;
+    matrix::DiagonalMatrix< std::complex< double > > omega_matrix_;
+
+    std::vector< double > penetrabilities_;
+    std::vector< double > shift_factors_;
+    std::vector< double > phase_shifts_;
+    std::vector< double > coulomb_phase_shifts_;
+    std::vector< double > boundaries_;
+    std::vector< bool > below_threshold_;
+
+    /* auxiliary functions */
+
+    // blatantly copied from Reich-Moore
+    #include "dryad/resonances/calculator/ReichMoore/src/selectLMatrix.hpp"
 
     /* auxiliary functions */
 
