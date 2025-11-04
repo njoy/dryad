2a822967783f1f042db3abba62f0696fe7cee2ee
Wim Haeck <whaeck@gmail.com>
2025-11-04 06:08:22 -0700

    Adding python bindings for the kinematics enum

--- python/stubs/dryad/resonances.pyi
+++ python/stubs/dryad/resonances.pyi
@@ -5,7 +5,7 @@ from __future__ import annotations
 import dryad
 import dryad.id
 import typing
-__all__: list[str] = ['BoundaryCondition', 'Channel', 'ChannelQuantumNumbers', 'ChannelRadii', 'CompoundSystem', 'CoulombPenetrability', 'CoulombPhaseShift', 'CoulombPhaseShiftDifference', 'CoulombShiftFactor', 'Formalism', 'HardSpherePenetrability', 'HardSpherePhaseShift', 'HardSphereShiftFactor', 'Particle', 'ParticlePair', 'ResonanceParameters', 'ResonanceTable', 'SpinGroup', 'TabulatedRadius', 'TabulatedWaveFunction']
+__all__: list[str] = ['BoundaryCondition', 'Channel', 'ChannelQuantumNumbers', 'ChannelRadii', 'CompoundSystem', 'CoulombPenetrability', 'CoulombPhaseShift', 'CoulombPhaseShiftDifference', 'CoulombShiftFactor', 'Formalism', 'HardSpherePenetrability', 'HardSpherePhaseShift', 'HardSphereShiftFactor', 'Kinematics', 'Particle', 'ParticlePair', 'ResonanceParameters', 'ResonanceTable', 'SpinGroup', 'TabulatedRadius', 'TabulatedWaveFunction']
 class BoundaryCondition:
     """
     The boundary condition options
@@ -844,6 +844,53 @@ class HardSphereShiftFactor:
         """
         The value of the orbital angular momentum
         """
+class Kinematics:
+    """
+    The kinematics option
+    
+    Members:
+    
+      NonRelativistic
+    
+      Relativistic
+    """
+    NonRelativistic: typing.ClassVar[Kinematics]  # value = <Kinematics.NonRelativistic: 0>
+    Relativistic: typing.ClassVar[Kinematics]  # value = <Kinematics.Relativistic: 1>
+    __members__: typing.ClassVar[dict[str, Kinematics]]  # value = {'NonRelativistic': <Kinematics.NonRelativistic: 0>, 'Relativistic': <Kinematics.Relativistic: 1>}
+    def __eq__(self, other: typing.Any) -> bool:
+        ...
+    def __ge__(self, other: typing.Any) -> bool:
+        ...
+    def __getstate__(self) -> int:
+        ...
+    def __gt__(self, other: typing.Any) -> bool:
+        ...
+    def __hash__(self) -> int:
+        ...
+    def __index__(self) -> int:
+        ...
+    def __init__(self, value: int) -> None:
+        ...
+    def __int__(self) -> int:
+        ...
+    def __le__(self, other: typing.Any) -> bool:
+        ...
+    def __lt__(self, other: typing.Any) -> bool:
+        ...
+    def __ne__(self, other: typing.Any) -> bool:
+        ...
+    def __repr__(self) -> str:
+        ...
+    def __setstate__(self, state: int) -> None:
+        ...
+    def __str__(self) -> str:
+        ...
+    @property
+    def name(self) -> str:
+        ...
+    @property
+    def value(self) -> int:
+        ...
 class Particle:
     """
     Particle information for resonance reconstruction
