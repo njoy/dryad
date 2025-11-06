39fde555a802a41a0874c6476412f8106dab4715
Wim Haeck <whaeck@gmail.com>
2025-11-06 11:59:05 -0700

    adding python bindings for medic's prune functions

--- python/src/njoy.python.cpp
+++ python/src/njoy.python.cpp
@@ -10,8 +10,9 @@ namespace python = pybind11;
 // declarations - matrix functions
 void wrapMatrix( python::module& );
 
-// declarations
+// declarations - components and modules
 void wrapDryad( python::module& );
+void wrapMedic( python::module& );
 
 /**
  *  @brief njoy python bindings
@@ -26,4 +27,7 @@ PYBIND11_MODULE( njoy, module ) {
 
   // wrap dryad
   wrapDryad( module );
+
+  // wrap medic
+  wrapMedic( module );
 }
