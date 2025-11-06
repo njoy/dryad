2c185ff497f6e2a13454ae1efab2364d53c024c1
Wim Haeck <whaeck@gmail.com>
2025-11-06 12:01:13 -0700

    starting to work on tests for pruning

---
+++ python/test/medic/Test_prune_cross_section.py
@@ -0,0 +1,20 @@
+# standard imports
+import unittest
+import sys
+
+# third party imports
+
+# local imports
+from njoy.medic import prune_cross_section
+from njoy.dryad import TabulatedCrossSection
+
+class Test_prune_cross_section( unittest.TestCase ) :
+    """Unit test for the prune_cross_section function."""
+
+    def test_component( self ) :
+
+        pass
+
+if __name__ == '__main__' :
+
+    unittest.main()
---
+++ python/test/medic/Test_prune_cross_sections.py
@@ -0,0 +1,20 @@
+# standard imports
+import unittest
+import sys
+
+# third party imports
+
+# local imports
+from njoy.medic import prune_cross_sections
+from njoy.dryad import ProjectileTarget
+
+class Test_prune_cross_sections( unittest.TestCase ) :
+    """Unit test for the prune_cross_sections function."""
+
+    def test_component( self ) :
+
+        pass
+
+if __name__ == '__main__' :
+
+    unittest.main()
