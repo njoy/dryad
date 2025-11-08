a4f21ba2f2a9047e38f5ec8f353414eeb24bc3ca
Wim Haeck <whaeck@gmail.com>
2025-11-08 08:53:33 -0700

    adding elecroatomic distribution blocks

--- cmake/unit_testing.cmake
+++ cmake/unit_testing.cmake
@@ -130,6 +130,9 @@ add_cpp_test( dryad.format.ace.createProjectileTargetFromFile                   
 
 add_cpp_test( dryad.format.ace.electroatomic.createAcePrincipalCrossSectionBlock        dryad/format/ace/electroatomic/createAcePrincipalCrossSectionBlock.test.cpp )
 add_cpp_test( dryad.format.ace.electroatomic.createAceElasticCrossSectionBlock          dryad/format/ace/electroatomic/createAceElasticCrossSectionBlock.test.cpp )
+add_cpp_test( dryad.format.ace.electroatomic.createAceElasticAngularDistributionBlock   dryad/format/ace/electroatomic/createAceElasticAngularDistributionBlock.test.cpp )
+add_cpp_test( dryad.format.ace.electroatomic.createAceBremsstrahlungDistributionBlock   dryad/format/ace/electroatomic/createAceBremsstrahlungDistributionBlock.test.cpp )
+add_cpp_test( dryad.format.ace.electroatomic.createAceIonisationDistributionBlocks      dryad/format/ace/electroatomic/createAceIonisationDistributionBlocks.test.cpp )
 add_cpp_test( dryad.format.ace.electroatomic.createAceExcitationBlock                   dryad/format/ace/electroatomic/createAceExcitationBlock.test.cpp )
 add_cpp_test( dryad.format.ace.electroatomic.createAceBremsstrahlungBlock               dryad/format/ace/electroatomic/createAceBremsstrahlungBlock.test.cpp )
 
---
+++ test/dryad/format/ace/electroatomic/createAceIonisationDistributionBlocks.test.cpp
@@ -0,0 +1,59 @@
+// include Catch2
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_floating_point.hpp>
+using Catch::Matchers::WithinRel;
+
+// what we are testing
+#include "njoy/dryad/format/ace/electroatomic/createAceIonisationDistributionBlocks.hpp"
+
+// other includes
+#include "njoy/dryad/format/endf/createProjectileTargetFromFile.hpp"
+
+// convenience typedefs
+using namespace njoy::dryad;
+
+SCENARIO( "createAceBremsstrahlungDistributionBlock" ) {
+
+  GIVEN( "instances of ProjectileTarget" ) {
+
+    WHEN( "correct data is given" ) {
+
+      auto electroatomic = format::endf::createProjectileTargetFromFile( "e-001_H_000.endf", true );
+      electroatomic.unioniseCrossSections();
+      electroatomic.calculateSummationCrossSections();
+
+      THEN( "the ace blocks can be generated" ) {
+
+        auto blocks = format::ace::electroatomic::createAceIonisationDistributionBlocks( electroatomic );
+
+        CHECK( 1 == blocks.size() );
+
+        auto block = blocks[0];
+        CHECK( 8 == block.numberEnergyPoints() );
+        CHECK( 8 == block.energies().size() );
+        CHECK( 8 == block.distributions().size() );
+
+        CHECK_THAT( 1.36e-5, WithinRel( block.energies().front(), 1e-12 ) );
+        CHECK_THAT( 1e+5, WithinRel( block.energies().back(), 1e-12 ) );
+
+        CHECK_THAT( 1.36e-5, WithinRel( block.distributions().front().energy(), 1e-12 ) );
+        CHECK( 2 == block.distributions().front().numberOutgoingEnergies() );
+        CHECK( 2 == block.distributions().front().outgoingEnergies().size() );
+        CHECK( 2 == block.distributions().front().cdf().size() );
+        CHECK_THAT( .00279866e-6, WithinRel( block.distributions().front().outgoingEnergies().front(), 1e-12 ) );
+        CHECK_THAT( .02798660e-6, WithinRel( block.distributions().front().outgoingEnergies().back(), 1e-12 ) );
+        CHECK_THAT( 0., WithinRel( block.distributions().front().cdf().front(), 1e-12 ) );
+        CHECK_THAT( 1., WithinRel( block.distributions().front().cdf().back(), 1e-12 ) );
+
+        CHECK_THAT( 1e+5, WithinRel( block.distributions().back().energy(), 1e-12 ) );
+        CHECK( 147 == block.distributions().back().numberOutgoingEnergies() );
+        CHECK( 147 == block.distributions().back().outgoingEnergies().size() );
+        CHECK( 147 == block.distributions().back().cdf().size() );
+        CHECK_THAT( 1e-7, WithinRel( block.distributions().back().outgoingEnergies().front(), 1e-12 ) );
+        CHECK_THAT( 5e+4, WithinRel( block.distributions().back().outgoingEnergies().back(), 1e-12 ) );
+        CHECK_THAT( 0., WithinRel( block.distributions().back().cdf().front(), 1e-12 ) );
+        CHECK_THAT( 1., WithinRel( block.distributions().back().cdf().back(), 1e-12 ) );
+      } // THEN
+    } // WHEN
+  } // GIVEN
+} // SCENARIO
