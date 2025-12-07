69e76da2dea0ae7fd7c51e551b88e0e811256a4d
Wim Haeck <whaeck@gmail.com>
2025-12-07 14:50:34 -0700

    fixing a few typos in the documentation

--- src/njoy/dryad/format/ace/photoatomic/createAceCoherentFormFactorBlock.hpp
+++ src/njoy/dryad/format/ace/photoatomic/createAceCoherentFormFactorBlock.hpp
@@ -17,7 +17,7 @@ namespace ace {
 namespace photoatomic {
 
   /**
-   *  @brief Create the coherent form factorblock for photoatomic data
+   *  @brief Create the coherent form factor block for photoatomic data
    *
    *  It is assumed that the ProjectileTarget instance used is valid photoatomic
    *  data, and ACE compatible (i.e. unionised energy grid, etc.)
