a95d50778dffffe6b165d455229a78f473ad6fcf
Wim Haeck <whaeck@gmail.com>
2026-01-13 04:33:08 -0700

    updating include files

--- src/njoy/dryad/thermal.hpp
+++ src/njoy/dryad/thermal.hpp
@@ -1,2 +1,4 @@
 // components
 #include "njoy/dryad/thermal/TabulatedDebyeWallerIntegral.hpp"
+#include "njoy/dryad/thermal/TabulatedEffectiveTemperature.hpp"
+#include "njoy/dryad/thermal/IncoherentElasticScattering.hpp"
