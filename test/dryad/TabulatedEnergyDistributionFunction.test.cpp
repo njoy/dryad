b65cc66d54f857b9a970fb0ff73f648e84b6163e
Wim Haeck <whaeck@gmail.com>
2026-01-25 17:47:39 -0700

    updating more tests

--- test/dryad/TabulatedEnergyDistributionFunction.test.cpp
+++ test/dryad/TabulatedEnergyDistributionFunction.test.cpp
@@ -1936,6 +1936,54 @@ SCENARIO( "TabulatedEnergyDistributionFunction" ) {
     } // WHEN
   } // GIVEN
 
+
+  GIVEN( "non-linearised data with multiple regions with a jump that consist of "
+         "more than 2 points" ) {
+
+    // note: at construction time, the extraneous points in between the first and last
+    //       x value in the jump are removed. boundaries always point to the first point
+    //       in the jump
+
+    WHEN( "the data is given explicitly" ) {
+
+      const std::vector< double > x = { 1., 2., 2., 2., 3., 4. }; // <-- jump of 3 x values
+      const std::vector< double > y = { 4., 3., 2., 4., 3., 2. };
+      const std::vector< std::size_t > boundaries = { 2, 5 }; // <-- pointing to middle of the jump
+      const std::vector< InterpolationType > interpolants = {
+
+        InterpolationType::LinearLinear,
+        InterpolationType::LinearLog
+      };
+
+      TabulatedEnergyDistributionFunction chunk( std::move( x ), std::move( y ),
+                                                 std::move( boundaries ),
+                                                 std::move( interpolants ) );
+
+      THEN( "a TabulatedEnergyDistributionFunction can be constructed and members can be tested" ) {
+
+        CHECK( 5 == chunk.energies().size() );
+        CHECK( 5 == chunk.values().size() );
+        CHECK( 2 == chunk.boundaries().size() );
+        CHECK( 2 == chunk.interpolants().size() );
+        CHECK_THAT( 1., WithinRel( chunk.energies()[0] ) );
+        CHECK_THAT( 2., WithinRel( chunk.energies()[1] ) );
+        CHECK_THAT( 2., WithinRel( chunk.energies()[2] ) );
+        CHECK_THAT( 3., WithinRel( chunk.energies()[3] ) );
+        CHECK_THAT( 4., WithinRel( chunk.energies()[4] ) );
+        CHECK_THAT( 4., WithinRel( chunk.values()[0] ) );
+        CHECK_THAT( 3., WithinRel( chunk.values()[1] ) );
+        CHECK_THAT( 4., WithinRel( chunk.values()[2] ) );
+        CHECK_THAT( 3., WithinRel( chunk.values()[3] ) );
+        CHECK_THAT( 2., WithinRel( chunk.values()[4] ) );
+        CHECK( 1 == chunk.boundaries()[0] );         // <-- this is changed from 2 to 1
+        CHECK( 4 == chunk.boundaries()[1] );         // <-- this is changed from 5 to 4
+        CHECK( InterpolationType::LinearLinear == chunk.interpolants()[0] );
+        CHECK( InterpolationType::LinearLog == chunk.interpolants()[1] );
+        CHECK( false == chunk.isLinearised() );
+      } // THEN
+    } // WHEN
+  } // GIVEN
+
   GIVEN( "non-linearised data with multiple regions with a jump and boundaries "
          "that point to the second x value in the jump" ) {
 
@@ -1983,7 +2031,7 @@ SCENARIO( "TabulatedEnergyDistributionFunction" ) {
     } // WHEN
   } // GIVEN
 
-  GIVEN( "non-linearised data with multiple regions with a jump at the end that goes to zero" ) {
+  GIVEN( "non-linearised data with multiple regions with a jump at the end" ) {
 
     // note: at construction time, the last x and y value will be removed and the last
     //       boundary value will be decremented by 1.
@@ -1991,7 +2039,7 @@ SCENARIO( "TabulatedEnergyDistributionFunction" ) {
     WHEN( "the data is given explicitly" ) {
 
       const std::vector< double > x = { 1., 2., 3., 4., 4. }; // <-- jump at end
-      const std::vector< double > y = { 4., 3., 2., 1., 0. }; // <-- last value is zero
+      const std::vector< double > y = { 4., 3., 2., 1., 4. };
       const std::vector< std::size_t > boundaries = { 1, 4 }; // <-- pointing to end
       const std::vector< InterpolationType > interpolants = {
 
@@ -2020,6 +2068,51 @@ SCENARIO( "TabulatedEnergyDistributionFunction" ) {
         CHECK_THAT( 2., WithinRel( chunk.values()[2] ) );
         CHECK_THAT( 1., WithinRel( chunk.values()[3] ) ); // <-- last point removed
         CHECK( 1 == chunk.boundaries()[0] );
+        CHECK( 3 == chunk.boundaries()[1] );         // <-- boundary value reset
+        CHECK( InterpolationType::LinearLinear == chunk.interpolants()[0] );
+        CHECK( InterpolationType::LinearLog == chunk.interpolants()[1] );
+        CHECK( false == chunk.isLinearised() );
+      } // THEN
+    } // WHEN
+  } // GIVEN
+
+  GIVEN( "non-linearised data with multiple regions with a jump at the beginning" ) {
+
+    // note: at construction time, the first x and y value will be removed and all
+    //       boundary values will be decremented by 1.
+
+    WHEN( "the data is given explicitly" ) {
+
+      const std::vector< double > x = { 1., 1., 2., 3., 4. }; // <-- jump at beginning
+      const std::vector< double > y = { 1., 4., 3., 2., 1. };
+      const std::vector< std::size_t > boundaries = { 2, 4 }; // <-- pointing to end
+      const std::vector< InterpolationType > interpolants = {
+
+        InterpolationType::LinearLinear,
+        InterpolationType::LinearLog
+      };
+
+      TabulatedEnergyDistributionFunction chunk( std::move( x ), std::move( y ),
+                                                 std::move( boundaries ),
+                                                 std::move( interpolants ) );
+
+      THEN( "an TabulatedEnergyDistributionFunction can be constructed and members can be tested" ) {
+
+        CHECK( 4 == chunk.numberPoints() );
+        CHECK( 2 == chunk.numberRegions() );
+        CHECK( 4 == chunk.energies().size() );
+        CHECK( 4 == chunk.values().size() );
+        CHECK( 2 == chunk.boundaries().size() );
+        CHECK( 2 == chunk.interpolants().size() );
+        CHECK_THAT( 1., WithinRel( chunk.energies()[0] ) ); // <-- first point removed
+        CHECK_THAT( 2., WithinRel( chunk.energies()[1] ) );
+        CHECK_THAT( 3., WithinRel( chunk.energies()[2] ) );
+        CHECK_THAT( 4., WithinRel( chunk.energies()[3] ) );
+        CHECK_THAT( 4., WithinRel( chunk.values()[0] ) ); // <-- first point removed
+        CHECK_THAT( 3., WithinRel( chunk.values()[1] ) );
+        CHECK_THAT( 2., WithinRel( chunk.values()[2] ) );
+        CHECK_THAT( 1., WithinRel( chunk.values()[3] ) );
+        CHECK( 1 == chunk.boundaries()[0] );         // <-- boundary value reset
         CHECK( 3 == chunk.boundaries()[1] );         // <-- boundary value reset
         CHECK( InterpolationType::LinearLinear == chunk.interpolants()[0] );
         CHECK( InterpolationType::LinearLog == chunk.interpolants()[1] );
@@ -2093,39 +2186,6 @@ SCENARIO( "TabulatedEnergyDistributionFunction" ) {
 
       std::vector< double > x = { 1., 3., 2., 4. };
       std::vector< double > y = { 4., 3., 2., 1. };
-
-      THEN( "an exception is thrown" ) {
-
-        CHECK_THROWS( TabulatedEnergyDistributionFunction( std::move( x ), std::move( y ) ) );
-      } // THEN
-    } // WHEN
-
-    WHEN( "the x grid contains a triple x value" ) {
-
-      std::vector< double > x = { 1., 2., 2., 2., 3., 4. };
-      std::vector< double > y = { 4., 3., 3., 3., 2., 1. };
-
-      THEN( "an exception is thrown" ) {
-
-        CHECK_THROWS( TabulatedEnergyDistributionFunction( std::move( x ), std::move( y ) ) );
-      } // THEN
-    } // WHEN
-
-    WHEN( "the x grid has a jump at the beginning" ) {
-
-      std::vector< double > x = { 1., 1., 3., 4. };
-      std::vector< double > y = { 4., 3., 1., 4. };
-
-      THEN( "an exception is thrown" ) {
-
-        CHECK_THROWS( TabulatedEnergyDistributionFunction( std::move( x ), std::move( y ) ) );
-      } // THEN
-    } // WHEN
-
-    WHEN( "the x grid has a jump at the end" ) {
-
-      std::vector< double > x = { 1., 2., 4., 4. };
-      std::vector< double > y = { 4., 3., 1., 4. };
 
       THEN( "an exception is thrown" ) {
 
