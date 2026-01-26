889507f3ebbaff1f2b3231b466647f9216ea0e96
Wim Haeck <whaeck@gmail.com>
2026-01-25 17:33:13 -0700

    updating tests for average energy and angles

--- test/dryad/TabulatedAverageCosine.test.cpp
+++ test/dryad/TabulatedAverageCosine.test.cpp
@@ -1892,7 +1892,101 @@ SCENARIO( "TabulatedAverageCosine" ) {
     } // WHEN
   } // GIVEN
 
-  GIVEN( "non-linearised data with multiple regions with a jump at the end that goes to zero" ) {
+  GIVEN( "non-linearised data with multiple regions with a jump that consist of "
+         "more than 2 points" ) {
+
+    // note: at construction time, the extraneous points in between the first and last
+    //       x value in the jump are removed. boundaries always point to the first point
+    //       in the jump
+
+    WHEN( "the data is given explicitly" ) {
+
+      const std::vector< double > x = { 1., 2., 2., 2., 3., 4. }; // <-- jump of 3 x values
+      const std::vector< double > y = { 4., 3., 2., 4., 3., 2. };
+      const std::vector< std::size_t > boundaries = { 2, 5 }; // <-- pointing to middle of the jump
+      const std::vector< InterpolationType > interpolants = {
+
+        InterpolationType::LinearLinear,
+        InterpolationType::LinearLog
+      };
+
+      TabulatedAverageCosine chunk( std::move( x ), std::move( y ),
+                                    std::move( boundaries ),
+                                    std::move( interpolants ) );
+
+      THEN( "a TabulatedAverageCosine can be constructed and members can be tested" ) {
+
+        CHECK( 5 == chunk.energies().size() );
+        CHECK( 5 == chunk.values().size() );
+        CHECK( 2 == chunk.boundaries().size() );
+        CHECK( 2 == chunk.interpolants().size() );
+        CHECK_THAT( 1., WithinRel( chunk.energies()[0] ) );
+        CHECK_THAT( 2., WithinRel( chunk.energies()[1] ) );
+        CHECK_THAT( 2., WithinRel( chunk.energies()[2] ) );
+        CHECK_THAT( 3., WithinRel( chunk.energies()[3] ) );
+        CHECK_THAT( 4., WithinRel( chunk.energies()[4] ) );
+        CHECK_THAT( 4., WithinRel( chunk.values()[0] ) );
+        CHECK_THAT( 3., WithinRel( chunk.values()[1] ) );
+        CHECK_THAT( 4., WithinRel( chunk.values()[2] ) );
+        CHECK_THAT( 3., WithinRel( chunk.values()[3] ) );
+        CHECK_THAT( 2., WithinRel( chunk.values()[4] ) );
+        CHECK( 1 == chunk.boundaries()[0] );         // <-- this is changed from 2 to 1
+        CHECK( 4 == chunk.boundaries()[1] );         // <-- this is changed from 5 to 4
+        CHECK( InterpolationType::LinearLinear == chunk.interpolants()[0] );
+        CHECK( InterpolationType::LinearLog == chunk.interpolants()[1] );
+        CHECK( false == chunk.isLinearised() );
+      } // THEN
+    } // WHEN
+  } // GIVEN
+
+  GIVEN( "non-linearised data with multiple regions with a jump and boundaries "
+         "that point to the second x value in the jump" ) {
+
+    // note: at construction time, the boundary value will be set to the first point in
+    //       the jump. As a result, the final data contained in this TabulatedAverageCosine is the
+    //       same as the previous test.
+
+    WHEN( "the data is given explicitly" ) {
+
+      const std::vector< double > x = { 1., 2., 2., 3., 4. };
+      const std::vector< double > y = { 4., 3., 4., 3., 2. };
+      const std::vector< std::size_t > boundaries = { 2, 4 }; // <-- pointing to end of the jump
+      const std::vector< InterpolationType > interpolants = {
+
+        InterpolationType::LinearLinear,
+        InterpolationType::LinearLog
+      };
+
+      TabulatedAverageCosine chunk( std::move( x ), std::move( y ),
+                                    std::move( boundaries ),
+                                    std::move( interpolants ) );
+
+      THEN( "a TabulatedAverageCosine can be constructed and members can be tested" ) {
+
+        CHECK( 5 == chunk.energies().size() );
+        CHECK( 5 == chunk.values().size() );
+        CHECK( 2 == chunk.boundaries().size() );
+        CHECK( 2 == chunk.interpolants().size() );
+        CHECK_THAT( 1., WithinRel( chunk.energies()[0] ) );
+        CHECK_THAT( 2., WithinRel( chunk.energies()[1] ) );
+        CHECK_THAT( 2., WithinRel( chunk.energies()[2] ) );
+        CHECK_THAT( 3., WithinRel( chunk.energies()[3] ) );
+        CHECK_THAT( 4., WithinRel( chunk.energies()[4] ) );
+        CHECK_THAT( 4., WithinRel( chunk.values()[0] ) );
+        CHECK_THAT( 3., WithinRel( chunk.values()[1] ) );
+        CHECK_THAT( 4., WithinRel( chunk.values()[2] ) );
+        CHECK_THAT( 3., WithinRel( chunk.values()[3] ) );
+        CHECK_THAT( 2., WithinRel( chunk.values()[4] ) );
+        CHECK( 1 == chunk.boundaries()[0] );           // <-- this is changed from 2 to 1
+        CHECK( 4 == chunk.boundaries()[1] );
+        CHECK( InterpolationType::LinearLinear == chunk.interpolants()[0] );
+        CHECK( InterpolationType::LinearLog == chunk.interpolants()[1] );
+        CHECK( false == chunk.isLinearised() );
+      } // THEN
+    } // WHEN
+  } // GIVEN
+
+  GIVEN( "non-linearised data with multiple regions with a jump at the end" ) {
 
     // note: at construction time, the last x and y value will be removed and the last
     //       boundary value will be decremented by 1.
@@ -1900,7 +1994,7 @@ SCENARIO( "TabulatedAverageCosine" ) {
     WHEN( "the data is given explicitly" ) {
 
       const std::vector< double > x = { 1., 2., 3., 4., 4. }; // <-- jump at end
-      const std::vector< double > y = { 4., 3., 2., 1., 0. }; // <-- last value is zero
+      const std::vector< double > y = { 4., 3., 2., 1., 4. };
       const std::vector< std::size_t > boundaries = { 1, 4 }; // <-- pointing to end
       const std::vector< InterpolationType > interpolants = {
 
@@ -1909,8 +2003,8 @@ SCENARIO( "TabulatedAverageCosine" ) {
       };
 
       TabulatedAverageCosine chunk( std::move( x ), std::move( y ),
-                                   std::move( boundaries ),
-                                   std::move( interpolants ) );
+                                    std::move( boundaries ),
+                                    std::move( interpolants ) );
 
       THEN( "an TabulatedAverageCosine can be constructed and members can be tested" ) {
 
@@ -1929,6 +2023,51 @@ SCENARIO( "TabulatedAverageCosine" ) {
         CHECK_THAT( 2., WithinRel( chunk.values()[2] ) );
         CHECK_THAT( 1., WithinRel( chunk.values()[3] ) ); // <-- last point removed
         CHECK( 1 == chunk.boundaries()[0] );
+        CHECK( 3 == chunk.boundaries()[1] );         // <-- boundary value reset
+        CHECK( InterpolationType::LinearLinear == chunk.interpolants()[0] );
+        CHECK( InterpolationType::LinearLog == chunk.interpolants()[1] );
+        CHECK( false == chunk.isLinearised() );
+      } // THEN
+    } // WHEN
+  } // GIVEN
+
+  GIVEN( "non-linearised data with multiple regions with a jump at the beginning" ) {
+
+    // note: at construction time, the first x and y value will be removed and all
+    //       boundary values will be decremented by 1.
+
+    WHEN( "the data is given explicitly" ) {
+
+      const std::vector< double > x = { 1., 1., 2., 3., 4. }; // <-- jump at beginning
+      const std::vector< double > y = { 1., 4., 3., 2., 1. };
+      const std::vector< std::size_t > boundaries = { 2, 4 }; // <-- pointing to end
+      const std::vector< InterpolationType > interpolants = {
+
+        InterpolationType::LinearLinear,
+        InterpolationType::LinearLog
+      };
+
+      TabulatedAverageCosine chunk( std::move( x ), std::move( y ),
+                                    std::move( boundaries ),
+                                    std::move( interpolants ) );
+
+      THEN( "an TabulatedAverageCosine can be constructed and members can be tested" ) {
+
+        CHECK( 4 == chunk.numberPoints() );
+        CHECK( 2 == chunk.numberRegions() );
+        CHECK( 4 == chunk.energies().size() );
+        CHECK( 4 == chunk.values().size() );
+        CHECK( 2 == chunk.boundaries().size() );
+        CHECK( 2 == chunk.interpolants().size() );
+        CHECK_THAT( 1., WithinRel( chunk.energies()[0] ) ); // <-- first point removed
+        CHECK_THAT( 2., WithinRel( chunk.energies()[1] ) );
+        CHECK_THAT( 3., WithinRel( chunk.energies()[2] ) );
+        CHECK_THAT( 4., WithinRel( chunk.energies()[3] ) );
+        CHECK_THAT( 4., WithinRel( chunk.values()[0] ) ); // <-- first point removed
+        CHECK_THAT( 3., WithinRel( chunk.values()[1] ) );
+        CHECK_THAT( 2., WithinRel( chunk.values()[2] ) );
+        CHECK_THAT( 1., WithinRel( chunk.values()[3] ) );
+        CHECK( 1 == chunk.boundaries()[0] );         // <-- boundary value reset
         CHECK( 3 == chunk.boundaries()[1] );         // <-- boundary value reset
         CHECK( InterpolationType::LinearLinear == chunk.interpolants()[0] );
         CHECK( InterpolationType::LinearLog == chunk.interpolants()[1] );
@@ -2002,39 +2141,6 @@ SCENARIO( "TabulatedAverageCosine" ) {
 
       std::vector< double > x = { 1., 3., 2., 4. };
       std::vector< double > y = { 4., 3., 2., 1. };
-
-      THEN( "an exception is thrown" ) {
-
-        CHECK_THROWS( TabulatedAverageCosine( std::move( x ), std::move( y ) ) );
-      } // THEN
-    } // WHEN
-
-    WHEN( "the x grid contains a triple x value" ) {
-
-      std::vector< double > x = { 1., 2., 2., 2., 3., 4. };
-      std::vector< double > y = { 4., 3., 3., 3., 2., 1. };
-
-      THEN( "an exception is thrown" ) {
-
-        CHECK_THROWS( TabulatedAverageCosine( std::move( x ), std::move( y ) ) );
-      } // THEN
-    } // WHEN
-
-    WHEN( "the x grid has a jump at the beginning" ) {
-
-      std::vector< double > x = { 1., 1., 3., 4. };
-      std::vector< double > y = { 4., 3., 1., 4. };
-
-      THEN( "an exception is thrown" ) {
-
-        CHECK_THROWS( TabulatedAverageCosine( std::move( x ), std::move( y ) ) );
-      } // THEN
-    } // WHEN
-
-    WHEN( "the x grid has a jump at the end" ) {
-
-      std::vector< double > x = { 1., 2., 4., 4. };
-      std::vector< double > y = { 4., 3., 1., 4. };
 
       THEN( "an exception is thrown" ) {
 
