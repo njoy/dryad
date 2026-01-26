a2220f425cebb00994f24b1837dc6454ec704bdf
Wim Haeck <whaeck@gmail.com>
2026-01-25 17:47:39 -0700

    updating more tests

--- python/test/dryad/Test_TabulatedEnergyDistributionFunction.py
+++ python/test/dryad/Test_TabulatedEnergyDistributionFunction.py
@@ -1731,18 +1731,36 @@ class Test_TabulatedEnergyDistributionFunction( unittest.TestCase ) :
 
         verify_chunk4( self, chunk )
 
-        # the data is given explicitly with boundaries that point to the second x value in the jump
-        chunk = TabulatedEnergyDistributionFunction( energies = [ 1., 2., 2., 3., 4. ],
-                                                     values = [ 4., 3., 4., 3., 2. ],
-                                                     boundaries = [ 2, 4 ],   # <-- pointing to end of the jump
+        # the data is given explicitly with a jump that uses more than 2 x values
+        chunk = TabulatedEnergyDistributionFunction( energies = [ 1., 2., 2., 2., 3., 4. ],
+                                                     values =  [ 4., 3., 2., 4., 3., 2. ],
+                                                     boundaries = [ 2, 5 ],   # <-- pointing to middle of the jump
                                                      interpolants = [ InterpolationType.LinearLinear,
-                                                                   InterpolationType.LinearLog ] )
+                                                                      InterpolationType.LinearLog ] )
 
         verify_chunk5( self, chunk )
 
-        # the data is given explicitly with a jump at the end that goes to zero
+        # the data is given explicitly with boundaries that point to the second x value in the jump
+        chunk = TabulatedEnergyDistributionFunction( energies = [ 1., 2., 2., 3., 4. ],
+                                                     values =  [ 4., 3., 4., 3., 2. ],
+                                                     boundaries = [ 2, 4 ],   # <-- pointing to end of the jump
+                                                     interpolants = [ InterpolationType.LinearLinear,
+                                                                      InterpolationType.LinearLog ] )
+
+        verify_chunk5( self, chunk )
+
+        # the data is given explicitly with a jump at the beginning
+        chunk = TabulatedEnergyDistributionFunction( energies = [ 1., 1., 2., 3., 4. ], # <-- jump at beginning
+                                                     values =  [ 1., 4., 3., 2., 1. ],
+                                                     boundaries = [ 2, 4 ],      # <-- pointing to end
+                                                     interpolants = [ InterpolationType.LinearLinear,
+                                                                      InterpolationType.LinearLog ] )
+
+        verify_chunk6( self, chunk )
+
+        # the data is given explicitly with a jump at the end
         chunk = TabulatedEnergyDistributionFunction( energies = [ 1., 2., 3., 4., 4. ], # <-- jump at end
-                                                     values = [ 4., 3., 2., 1., 0. ], # <-- last value is zero
+                                                     values =  [ 4., 3., 2., 1., 4. ],
                                                      boundaries = [ 1, 4 ],      # <-- pointing to end
                                                      interpolants = [ InterpolationType.LinearLinear,
                                                                       InterpolationType.LinearLog ] )
@@ -1795,24 +1813,6 @@ class Test_TabulatedEnergyDistributionFunction( unittest.TestCase ) :
 
             chunk = TabulatedEnergyDistributionFunction( energies = [ 1., 3., 2., 4. ],
                                                          values = [ 4., 3., 2., 1. ] )
-
-        # the x grid contains a triple x value
-        with self.assertRaises( Exception ) :
-
-            chunk = TabulatedEnergyDistributionFunction( energies = [ 1., 2., 2., 2., 3., 4. ],
-                                                         values = [ 4., 3., 3., 3., 2., 1. ] )
-
-        # the x grid has a jump at the beginning
-        with self.assertRaises( Exception ) :
-
-            chunk = TabulatedEnergyDistributionFunction( energies = [ 1., 1., 3., 4. ],
-                                                         values = [ 4., 3., 1., 4. ] )
-
-        # the x grid has a jump at the end
-        with self.assertRaises( Exception ) :
-
-            chunk = TabulatedEnergyDistributionFunction( energies = [ 1., 2., 4., 4. ],
-                                                         values = [ 4., 3., 1., 4. ] )
 
         # the last boundary does not point to the last point
         with self.assertRaises( Exception ) :
