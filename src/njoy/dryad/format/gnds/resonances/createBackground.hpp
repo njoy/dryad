e69fcaaa91ebe22d47e4013a350c7d6136e1994c
Wim Haeck <whaeck@gmail.com>
2026-02-01 10:42:22 -0700

    adding a gnds create function for external r matrix backgrounds

---
+++ src/njoy/dryad/format/gnds/resonances/createBackground.hpp
@@ -0,0 +1,97 @@
+#ifndef NJOY_DRYAD_FORMAT_GNDS_RESONANCES_CREATETABULATEDRADIUS
+#define NJOY_DRYAD_FORMAT_GNDS_RESONANCES_CREATETABULATEDRADIUS
+
+// system includes
+#include <variant>
+
+// other includes
+#include "pugixml.hpp"
+#include "tools/Log.hpp"
+#include "njoy/dryad/format/gnds/readDouble.hpp"
+#include "njoy/dryad/format/gnds/convertEnergy.hpp"
+#include "njoy/dryad/format/gnds/convertInverseEnergy.hpp"
+#include "njoy/dryad/format/gnds/convertInverseEnergySquared.hpp"
+#include "njoy/dryad/resonances/FrohnerBackground.hpp"
+#include "njoy/dryad/resonances/SammyBackground.hpp"
+#include "njoy/dryad/resonances/TabulatedBackground.hpp"
+
+namespace njoy {
+namespace dryad {
+namespace format {
+namespace gnds {
+namespace resonances {
+
+  /**
+   *  @brief Create a SammyBackground from a GNDS externalRMatrix node
+   *
+   *  @param[in] node   the GNDS XYs1d or regions1d node
+   */
+  inline std::variant< njoy::dryad::resonances::FrohnerBackground,
+                       njoy::dryad::resonances::SammyBackground,
+                       njoy::dryad::resonances::TabulatedBackground >
+  createBackground( const pugi::xml_node& external ) {
+
+    // check that this is a valid externalRMatrix node
+    throwExceptionOnWrongNode( external, "externalRMatrix" );
+
+    // get the type of external r-matrix element
+    auto type = external.attribute( "type" );
+    if ( type ) {
+
+      if ( strcmp( type.as_string(), "Froehner" ) == 0 ) {
+
+        auto r = readDouble( external.find_child_by_attribute( "double", "label", "constantExternalR" ) );
+        auto s = readDouble( external.find_child_by_attribute( "double", "label", "poleStrength" ) );
+        auto gamma = readDouble( external.find_child_by_attribute( "double", "label", "averageRadiationWidth" ) );
+        convertEnergy( gamma.first, gamma.second.value() );
+
+        auto lower = readDouble( external.find_child_by_attribute( "double", "label", "singularityEnergyBelow" ) );
+        auto upper = readDouble( external.find_child_by_attribute( "double", "label", "singularityEnergyAbove" ) );
+        convertEnergy( lower.first, lower.second.value() );
+        convertEnergy( upper.first, upper.second.value() );
+        return njoy::dryad::resonances::FrohnerBackground(
+                 r.first, s.first, gamma.first,
+                 lower.first, upper.first );
+      }
+      else if ( strcmp( type.as_string(), "SAMMY" ) == 0 ) {
+
+        auto r0 = readDouble( external.find_child_by_attribute( "double", "label", "constantExternalR" ) );
+        auto r1 = readDouble( external.find_child_by_attribute( "double", "label", "linearExternalR" ) );
+        auto r2 = readDouble( external.find_child_by_attribute( "double", "label", "quadraticExternalR" ) );
+        std::array< double, 3 > polynomial = { r0.first, r1.first, r2.first };
+        convertInverseEnergy( polynomial[1], r1.second.value() );
+        convertInverseEnergySquared( polynomial[2], r2.second.value() );
+
+        auto s0 = readDouble( external.find_child_by_attribute( "double", "label", "constantLogarithmicCoefficient" ) );
+        auto s1 = readDouble( external.find_child_by_attribute( "double", "label", "linearLogarithmicCoefficient" ) );
+        std::array< double, 2 > logarithmic = { s0.first, s1.first };
+        convertInverseEnergy( logarithmic[1], s1.second.value() );
+
+        auto lower = readDouble( external.find_child_by_attribute( "double", "label", "singularityEnergyBelow" ) );
+        auto upper = readDouble( external.find_child_by_attribute( "double", "label", "singularityEnergyAbove" ) );
+        convertEnergy( lower.first, lower.second.value() );
+        convertEnergy( upper.first, upper.second.value() );
+        return njoy::dryad::resonances::SammyBackground(
+                 std::move( polynomial ), std::move( logarithmic ),
+                 lower.first, upper.first );
+      }
+      else {
+
+        Log::error( "Encountered an unknown external R-matrix type: \'{}\'", type.as_string() );
+        throw std::exception();
+      }
+    }
+    else {
+
+      Log::error( "No type defined on the externalRMatrix node" );
+      throw std::exception();
+    }
+  }
+
+} // resonances namespace
+} // gnds namespace
+} // format namespace
+} // dryad namespace
+} // njoy namespace
+
+#endif
---
+++ test/dryad/format/gnds/resonances/createBackground.test.cpp
@@ -0,0 +1,58 @@
+// include Catch2
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_floating_point.hpp>
+using Catch::Matchers::WithinRel;
+
+// what we are testing
+#include "njoy/dryad/format/gnds/resonances/createBackground.hpp"
+
+// other includes
+#include "pugixml.hpp"
+
+// convenience typedefs
+using namespace njoy::dryad;
+using namespace njoy::dryad::resonances;
+using Background = std::variant< FrohnerBackground,
+                                 SammyBackground,
+                                 TabulatedBackground >;
+
+void verifySammyBackgroundChunk( const Background& );
+
+SCENARIO( "createBackground" ) {
+
+  GIVEN( "GNDS external R-matrix node - SAMMY parametrisation" ) {
+
+    pugi::xml_document document;
+    pugi::xml_parse_result result = document.load_file( "n-038_Sr_088.endf.gnds.xml" );
+    pugi::xml_node external = document.child( "reactionSuite" ).
+                                       child( "resonances" ).child( "resolved" ).
+                                       child( "RMatrix" ).child( "spinGroups" ).
+                                       child( "spinGroup" ).child( "channels" ).
+                                       find_child_by_attribute( "channel", "label", "1" ).
+                                       child( "externalRMatrix" );
+
+    WHEN( "a single external R-matrix node" ) {
+
+      THEN( "it can be converted" ) {
+
+        auto chunk = format::gnds::resonances::createBackground( external );
+
+        verifySammyBackgroundChunk( chunk );
+      } // THEN
+    } // WHEN
+  } // GIVEN
+} // SCENARIO
+
+void verifySammyBackgroundChunk( const Background& chunk ) {
+
+  CHECK( true == std::holds_alternative< SammyBackground >( chunk ) );
+
+  auto background = std::get< SammyBackground >( chunk );
+  CHECK_THAT( -0.043, WithinRel( background.polynomialCoefficients()[0] ) );
+  CHECK_THAT( 2.8e-8, WithinRel( background.polynomialCoefficients()[1] ) );
+  CHECK_THAT( 0.    , WithinRel( background.polynomialCoefficients()[2] ) );
+  CHECK_THAT( 0.01  , WithinRel( background.logarithmicCoefficients()[0] ) );
+  CHECK_THAT( 0.    , WithinRel( background.logarithmicCoefficients()[1] ) );
+  CHECK_THAT( 0.    , WithinRel( background.lowerSingularity() ) );
+  CHECK_THAT( 9.55e5, WithinRel( background.upperSingularity() ) );
+}
