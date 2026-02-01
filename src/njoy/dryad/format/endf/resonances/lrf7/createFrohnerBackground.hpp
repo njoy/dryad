cd2b754d89b1c658f254afd1fca85eaa748bf9a6
Wim Haeck <whaeck@gmail.com>
2026-02-01 09:07:56 -0700

    adding a Frohner create function for endf

---
+++ src/njoy/dryad/format/endf/resonances/lrf7/createFrohnerBackground.hpp
@@ -0,0 +1,52 @@
+#ifndef NJOY_DRYAD_FORMAT_ENDF_CREATETABULATEDBACKGROUND
+#define NJOY_DRYAD_FORMAT_ENDF_CREATETABULATEDBACKGROUND
+
+// system includes
+#include <vector>
+
+// other includes
+#include "tools/Log.hpp"
+#include "njoy/dryad/resonances/FrohnerBackground.hpp"
+#include "ENDFtk/section/2/151.hpp"
+
+namespace njoy {
+namespace dryad {
+namespace format {
+namespace endf {
+namespace resonances {
+namespace lrf7 {
+
+  /**
+   *  @brief Create a FrohnerBackground from a parsed ENDF section
+   *
+   *  @param[in] background   the energy dependent scattering radius record
+   */
+  inline dryad::resonances::FrohnerBackground
+  createFrohnerBackground(
+      const ENDFtk::section::Type< 2, 151 >::RMatrixLimited::FrohnerBackgroundRMatrix& background ) {
+
+    try {
+
+      Log::info( "Reading channel background parametrisation (Frohner)" );
+      double level = background.R0();
+      double pole = background.S0();
+      double width = background.GA();
+      double lower = background.ED();
+      double upper = background.EU();
+      return dryad::resonances::FrohnerBackground( level, pole, width, lower, upper );
+    }
+    catch ( ... ) {
+
+      Log::info( "Error encountered while creating a Frohner channel background parametrisation" );
+      throw;
+    }
+  }
+
+} // lrf7 namespace
+} // resonances namespace
+} // endf namespace
+} // format namespace
+} // dryad namespace
+} // njoy namespace
+
+#endif
