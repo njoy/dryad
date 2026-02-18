68233a0126da2abe1f34dda00a0fbe65afb9c3aa
Wim Haeck <whaeck@gmail.com>
2026-02-18 12:18:46 -0700

    adding external data: klein-nishina formula

---
+++ test/dryad/external/KleinNishina.test.cpp
@@ -0,0 +1,21 @@
+// include Catch2
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_floating_point.hpp>
+using Catch::Matchers::WithinRel;
+
+// what we are testing
+#include "njoy/dryad/external/KleinNishina.hpp"
+
+// other includes
+
+// convenience typedefs
+using namespace njoy::dryad::external;
+
+SCENARIO( "KleinNishina" ) {
+
+  GIVEN( "valid orbital momentum values for a HardSpherePenetrability" ) {
+
+//! @todo finish test
+
+  } // GIVEN
+} // SCENARIO
