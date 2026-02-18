38984d7e607b47a6ce18449c5e88d7d4a0292005
Wim Haeck <whaeck@gmail.com>
2026-02-18 16:32:50 -0700

    addign tests for 4 element types, using ENDF

--- test/acer/processElectronPhotonRelaxation.test.cpp
+++ test/acer/processElectronPhotonRelaxation.test.cpp
@@ -16,7 +16,9 @@ using namespace njoy::dryad;
 
 SCENARIO( "processElectronPhotonRelaxation" ) {
 
-  GIVEN( "" ) {
+  GIVEN( "instances of ProjectileTarget and AtomicRelaxation with z < 12" ) {
+
+    WHEN( "correct data is given" ) {
 
       auto relaxation = format::endf::createAtomicRelaxationFromFile( "atom-001_H_000.endf", true );
       relaxation.calculateTransitionEnergies();
@@ -31,8 +33,89 @@ SCENARIO( "processElectronPhotonRelaxation" ) {
 
       std::string filename = "fuvnrkljnvoiarnglkanrlknfalkvnlkrua.ace";
 
-      processElectronPhotonRelaxation( photoatomic, electroatomic, relaxation, filename );
+      THEN( "the ace file can be generated" ) {
 
-      std::remove( filename.c_str() );
+        processElectronPhotonRelaxation( photoatomic, electroatomic, relaxation, filename );
+
+//      std::remove( filename.c_str() );
+      } // THEN
+    } // WHEN
+  } // GIVEN
+
+  GIVEN( "instances of ProjectileTarget and AtomicRelaxation with z >= 12 and z < 31" ) {
+
+    WHEN( "correct data is given" ) {
+
+      auto relaxation = format::endf::createAtomicRelaxationFromFile( "atom-029_Cu_000.endf", true );
+      relaxation.calculateTransitionEnergies();
+      auto photoatomic = format::endf::createProjectileTargetFromFile( "photoat-029_Cu_000.endf", true );
+      photoatomic.unioniseCrossSections();
+      photoatomic.calculateSummationCrossSections();
+      photoatomic.calculateAverageEnergy();
+      external::ComptonProfiles::apply( photoatomic, true );
+      auto electroatomic = format::endf::createProjectileTargetFromFile( "e-029_Cu_000.endf", true );
+      electroatomic.unioniseCrossSections();
+      electroatomic.calculateSummationCrossSections();
+
+      std::string filename = "ajrhctvnoiweuynckeuhrckeuhwkrlk.ace";
+
+      THEN( "the ace file can be generated" ) {
+
+        processElectronPhotonRelaxation( photoatomic, electroatomic, relaxation, filename );
+
+//      std::remove( filename.c_str() );
+      } // THEN
+    } // WHEN
+  } // GIVEN
+
+  GIVEN( "instances of ProjectileTarget and AtomicRelaxation with z >= 31 and z < 36" ) {
+
+    WHEN( "correct data is given" ) {
+
+      auto relaxation = format::endf::createAtomicRelaxationFromFile( "atom-032_Ge_000.endf", true );
+      relaxation.calculateTransitionEnergies();
+      auto photoatomic = format::endf::createProjectileTargetFromFile( "photoat-032_Ge_000.endf", true );
+      photoatomic.unioniseCrossSections();
+      photoatomic.calculateSummationCrossSections();
+      photoatomic.calculateAverageEnergy();
+      external::ComptonProfiles::apply( photoatomic, true );
+      auto electroatomic = format::endf::createProjectileTargetFromFile( "e-032_Ge_000.endf", true );
+      electroatomic.unioniseCrossSections();
+      electroatomic.calculateSummationCrossSections();
+
+      std::string filename = "oieucnroieurcnouwehxmfkhskue4grkuegfl.ace";
+
+      THEN( "the ace file can be generated" ) {
+
+        processElectronPhotonRelaxation( photoatomic, electroatomic, relaxation, filename );
+
+//      std::remove( filename.c_str() );
+      } // THEN
+    } // WHEN
+  } // GIVEN
+
+  GIVEN( "instances of ProjectileTarget and AtomicRelaxation with z >= 37" ) {
+
+    WHEN( "correct data is given" ) {
+
+      auto relaxation = format::endf::createAtomicRelaxationFromFile( "atom-094_Pu_000.endf", true );
+      relaxation.calculateTransitionEnergies();
+      auto photoatomic = format::endf::createProjectileTargetFromFile( "photoat-094_Pu_000.endf", true );
+      photoatomic.unioniseCrossSections();
+      photoatomic.calculateSummationCrossSections();
+      photoatomic.calculateAverageEnergy();
+      external::ComptonProfiles::apply( photoatomic, true );
+      auto electroatomic = format::endf::createProjectileTargetFromFile( "e-094_Pu_000.endf", true );
+      electroatomic.unioniseCrossSections();
+      electroatomic.calculateSummationCrossSections();
+
+      std::string filename = "oxwincaweuxlkeuhrfkclalhnriuheuchaiulhrlm.ace";
+
+      THEN( "the ace file can be generated" ) {
+
+        processElectronPhotonRelaxation( photoatomic, electroatomic, relaxation, filename );
+//      std::remove( filename.c_str() );
+      } // THEN
+    } // WHEN
   } // GIVEN
 } // SCENARIO
