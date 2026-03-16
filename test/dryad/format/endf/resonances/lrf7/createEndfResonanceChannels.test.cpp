323d559cf87c64959b4a61bf9a57df0cef8b6bee
Wim Haeck <whaeck@gmail.com>
2026-03-16 12:11:29 -0600

    updating and testing creating endf resonance channeld

--- test/dryad/format/endf/resonances/lrf7/createEndfResonanceChannels.test.cpp
+++ test/dryad/format/endf/resonances/lrf7/createEndfResonanceChannels.test.cpp
@@ -5,38 +5,62 @@ using Catch::Matchers::WithinRel;
 
 // what we are testing
 #include "njoy/dryad/format/endf/resonances/lrf7/createEndfResonanceChannels.hpp"
-#include "njoy/dryad/format/endf/resonances/lrf7/createEndfParticlePairs.hpp"
 
 // other includes
-#include "ENDFtk/tree/fromFile.hpp"
-#include <iostream>
-#include "../../test.dryadcoumpound.hpp"
+#include "njoy/constants.hpp"
+
 // convenience typedefs
-using namespace njoy;
 using namespace njoy::dryad;
 using namespace njoy::dryad::resonances;
 
 std::string chunk();
 
-//! @todo test KBK and KPS
-
 SCENARIO( "createEndfResonanceChannels" ) {
 
-  GIVEN( "valid data for a reaction" ) {
+  GIVEN( "valid data for s spin group and particle pairs" ) {
 
     std::string string = chunk();
 
-    auto compound = create_dryad_compound();
+    WHEN( "the data is given explicitly" ) {
 
-    auto spingroup1 = create_dryad_spingroup_1();
+      auto photon = id::ParticleID::photon();
+      auto neutron = id::ParticleID::neutron();
+      auto proton = id::ParticleID::proton();
+      auto cu63 = id::ParticleID( "Cu63" );
+      auto cu64 = id::ParticleID( "Cu64[all]" );
 
-    WHEN( "the data is given explicitly" ) {       
+      ParticlePair photon_pair( Particle( photon, 0, 1, +1 ),
+                                Particle( cu64, 63.389 * njoy::constants::neutron_mass, 0, +1 ) );
+      ParticlePair neutron_pair( Particle( neutron, njoy::constants::neutron_mass, 0.5, +1 ),
+                                 Particle( cu63, 62.389 * njoy::constants::neutron_mass, 1.5, -1 ) );
+
+      ChannelRadii zero_radii( 0., 0. );
+      ChannelRadii radii( 6.7, 6.7 );
+
+      SpinGroup group( { { id::ChannelID( "n,Cu63->g,Cu64[all]{0,0,1+}" ),
+                           neutron_pair, photon_pair, 0., 0, zero_radii },
+                         { id::ChannelID( "n,Cu63->n,Cu63{1,1,1+}" ),
+                           neutron_pair, neutron_pair, 0., -1, radii },
+                         { id::ChannelID( "n,Cu63->n,Cu63{1,2,1+}" ),
+                           neutron_pair, neutron_pair, 0., -1, radii } },
+                       { { id::ChannelID( "n,Cu63->g,Cu64[all]{0,0,1+}" ),
+                           id::ChannelID( "n,Cu63->n,Cu63{1,1,1+}" ),
+                           id::ChannelID( "n,Cu63->n,Cu63{1,2,1+}" ) },
+                         { 3.811148e+3, 2.993903e+5 },
+                         { { 1., 2. },
+                           { 1.1, 2.1 },
+                           { 1.2, 2.2 } } },
+                       Formalism::ReichMoore,
+                       BoundaryCondition::ShiftFactor );
+
+      njoy::ENDFtk::section::Type< 2, 151 >::RMatrixLimited::ParticlePairs
+      pairs( { 0., 1. }, { 63.389, 62.389 }, { 0., 0. }, { 29., 29. },
+             { 1., 0.5 }, { 0., 1.5 }, { +1, +1 }, { +1, -1 },
+             { 0., 0. }, { -1, 1 }, { 0, 0 }, { 102, 2 } );
 
       THEN( "it can be converted to ENDF" ) {
 
-        auto ppairs = format::endf::resonances::lrf7::createEndfParticlePairs( compound );
-
-        auto data = format::endf::resonances::lrf7::createEndfResonanceChannels( spingroup1, ppairs );
+        auto data = format::endf::resonances::lrf7::createEndfResonanceChannels( group, pairs );
 
         std::string buffer;
         auto output = std::back_inserter( buffer );
@@ -50,9 +74,8 @@ SCENARIO( "createEndfResonanceChannels" ) {
 
 std::string chunk() {
   return
-    "-1.000000+0-1.000000+0          0          0         12          22925 2151     \n"
-    " 1.000000+0 1.000000+0 2.000000+0 1.100000+1 6.100000-1 6.200000-12925 2151     \n"
-    " 2.000000+0 3.000000+0 4.000000+0 1.200000+1 6.300000-1 6.400000-12925 2151     \n";
+    " 1.000000+0 1.000000+0          0          0         18          32925 2151     \n"
+    " 1.000000+0 0.000000+0 0.000000+0 0.000000+0 0.000000+0 0.000000+02925 2151     \n"
+    " 2.000000+0 1.000000+0 1.000000+0-1.000000+0 6.700000-1 6.700000-12925 2151     \n"
+    " 2.000000+0 1.000000+0 2.000000+0-1.000000+0 6.700000-1 6.700000-12925 2151     \n";
 }
-
-// if boundary is 1 or 2, clqsh, spin group boundary ? 
