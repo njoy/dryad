1d479e48d6216d7484e24545695581e1edb52f7f
Wim Haeck <whaeck@gmail.com>
2026-03-16 20:43:24 -0600

    adding a convenience constructor for resonance table

--- src/njoy/dryad/resonances/ResonanceTable/src/ctor.hpp
+++ src/njoy/dryad/resonances/ResonanceTable/src/ctor.hpp
@@ -33,8 +33,8 @@ public:
  *  The energies and channels do not have to be sorted (they will be sorted
  *  upon construction).
  *
- *  @param[in] channels     the channel identifiers (nc values, at least 1)
- *  @param[in] energies     the level energies (ne values, at least 1)
+ *  @param[in] channels     the channel identifiers (nc values)
+ *  @param[in] energies     the level energies (ne values)
  *  @param[in] amplitudes   the reduced width amplitudes (nc arrays of ne values)
  */
 ResonanceTable( std::vector< id::ChannelID > channels,
@@ -59,3 +59,23 @@ ResonanceTable( id::ChannelID channel,
     ResonanceTable( std::vector< id::ChannelID >{ std::move( channel ) },
                     std::move( energies ),
                     std::vector< std::vector< double > >{ std::move( amplitudes ) } ) {}
+
+/**
+ *  @brief Constructor for an empty table
+ *
+ *  @param[in] channels     the channel identifiers (nc values)
+ */
+ResonanceTable( std::vector< id::ChannelID > channels ) :
+    ResonanceTable( processTable( std::move( channels ) ) ) {}
+
+/**
+ *  @brief Constructor
+ *
+ *  The energies do not have to be sorted (they will be sorted upon construction).
+ *
+ *  @param[in] channel      the channel identifier
+ *  @param[in] energies     the level energies
+ *  @param[in] amplitudes   the reduced width amplitudes
+ */
+ResonanceTable( id::ChannelID channel ) :
+    ResonanceTable( std::vector< id::ChannelID >{ std::move( channel ) } ) {}
