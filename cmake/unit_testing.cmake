58604ad8ea28481ef93feec1f4fb27db1d5fb087
Wim Haeck <whaeck@gmail.com>
2026-04-01 15:02:06 -0600

    adding a read function for integer nodes

--- cmake/unit_testing.cmake
+++ cmake/unit_testing.cmake
@@ -277,6 +277,7 @@ add_cpp_test( dryad.format.gnds.createParticleIdentifier                        
 add_cpp_test( dryad.format.gnds.readShape                                               dryad/format/gnds/readShape.test.cpp )
 add_cpp_test( dryad.format.gnds.resolveLink                                             dryad/format/gnds/resolveLink.test.cpp )
 add_cpp_test( dryad.format.gnds.readDouble                                              dryad/format/gnds/readDouble.test.cpp )
+add_cpp_test( dryad.format.gnds.readInteger                                             dryad/format/gnds/readInteger.test.cpp )
 add_cpp_test( dryad.format.gnds.readAxis                                                dryad/format/gnds/readAxis.test.cpp )
 add_cpp_test( dryad.format.gnds.readGrid                                                dryad/format/gnds/readGrid.test.cpp )
 add_cpp_test( dryad.format.gnds.readAxes                                                dryad/format/gnds/readAxes.test.cpp )
