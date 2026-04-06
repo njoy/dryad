4276266ee8a60e8a7679f416f6070de6c6800678
Wim Haeck <whaeck@gmail.com>
2026-04-06 13:29:02 -0600

    it should be the max l value, not the number of l values

--- src/njoy/dryad/format/endf/resonances/lrf3/createSpinGroups.hpp
+++ src/njoy/dryad/format/endf/resonances/lrf3/createSpinGroups.hpp
@@ -42,7 +42,7 @@ namespace lrf3 {
     // create all possible channel quantum numbers
     auto spin = endf.spin();
     auto nls = endf.numberLValues();
-    auto available = dryad::resonances::ChannelQuantumNumbers::allowedChannelQuantumNumbers( 0.5, spin, nls );
+    auto available = dryad::resonances::ChannelQuantumNumbers::allowedChannelQuantumNumbers( 0.5, spin, nls - 1 );
 
     // the incident particle pair
     auto awri = endf.lValues().front().atomicWeightRatio();
