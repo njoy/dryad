1b9e65a99bf1062adba87a6fe042acf0b5091d89
Wim Haeck <whaeck@gmail.com>
2026-04-22 00:51:57 -0600

    adding a function to convert a string representing a fraction to a double

---
+++ test/dryad/format/gnds/readFractionFromString.test.cpp
@@ -0,0 +1,30 @@
+// include Catch2
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_floating_point.hpp>
+using Catch::Matchers::WithinRel;
+
+// what we are testing
+#include "njoy/dryad/format/gnds/readFractionFromString.hpp"
+
+// other includes
+#include "pugixml.hpp"
+
+// convenience typedefs
+using namespace njoy::dryad;
+
+SCENARIO( "readFractionFromString" ) {
+
+  GIVEN( "fraction strings" ) {
+
+    THEN( "they can be converted" ) {
+
+      CHECK_THAT( 0.5, WithinRel( format::gnds::readFractionFromString( "1/2" ) ) );
+      CHECK_THAT( 0.5, WithinRel( format::gnds::readFractionFromString( " 1 / 2 " ) ) );
+      CHECK_THAT( 0.5, WithinRel( format::gnds::readFractionFromString( "1 / 2" ) ) );
+      CHECK_THAT( 2.0, WithinRel( format::gnds::readFractionFromString( "2" ) ) );
+
+      CHECK_THROWS( format::gnds::readFractionFromString( "2.0" ) );
+      CHECK_THROWS( format::gnds::readFractionFromString( "1/2/3" ) );
+    } // THEN
+  } // GIVEN
+} // SCENARIO
