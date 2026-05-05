549d0c05189da60bd14b208a5a8a9eed08c9c1e9
Wim Haeck <whaeck@gmail.com>
2026-05-05 07:43:22 -0600

    setting very small eigenvalues (within 10 times epsilon) to 0

--- src/njoy/dryad/covariance/base/CovarianceMatrix/src/calculateEigenvalues.hpp
+++ src/njoy/dryad/covariance/base/CovarianceMatrix/src/calculateEigenvalues.hpp
@@ -4,6 +4,9 @@
  *  The eigenvalues can only be calculated from matrices on the diagonal
  *  of the full matrix. When this function is called on an off diagonal matrix,
  *  the function has no effect.
+ *
+ *  When the absolute value of an eigenvalue is within 10 times epsilon, the eigenvalue
+ *  will get set to zero exactly.
  */
 void calculateEigenvalues() {
 
@@ -17,7 +20,14 @@ void calculateEigenvalues() {
 
     for ( const auto& value : solver.eigenvalues().reshaped() ) {
 
-      eigenvalues.emplace_back( value );
+      if ( scion::math::isCloseToZero( value, 10. * std::numeric_limits< double >::epsilon() ) ) {
+
+        eigenvalues.emplace_back( 0. );
+      }
+      else {
+
+        eigenvalues.emplace_back( value );
+      }
     }
 
     this->eigenvalues_ = eigenvalues;
