a714e066045c65cfde74cfb620d4a48b396fa4ad
Alec William Golas <awgolas@lanl.gov>
2026-05-07 10:58:21 -0600

    Added tabulated level spacing python bindings

---
+++ python/src/dryad/resonances/TabulatedLevelSpacing.python.cpp
@@ -0,0 +1,112 @@
+// system includes
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+
+// local includes
+#include "dryad/definitions.hpp"
+#include "njoy/dryad/resonances/TabulatedLevelSpacing.hpp"
+
+// namespace aliases
+namespace python = pybind11;
+
+namespace dryad {
+namespace resonances {
+
+void wrapTabulatedLevelSpacing( python::module& module ) {  
+    
+  // type aliases
+  using Component = njoy::dryad::resonances::TabulatedLevelSpacing;
+  using InterpolationType = njoy::dryad::InterpolationType;
+
+  // wrap views created by this component
+
+  // create the component
+  python::class_< Component > component(
+
+    module,
+    "TabulatedLevelSpacing",
+    "A level spacing table"
+  );
+
+  // wrap the component
+  component
+  .def(
+
+    python::init< std::vector< double >,
+                  std::vector< double >,
+                  std::vector< std::size_t >,
+                  std::vector< InterpolationType > >(),
+    python::arg( "energies" ), python::arg( "values" ),
+    python::arg( "boundaries" ), python::arg( "interpolants" ),
+    "Initialise the level spacing table\n\n"
+    "Arguments:\n"
+    "    self           the level spacing table\n"
+    "    energies       the energy values\n"
+    "    values         the level spacing values\n"
+    "    boundaries     the boundaries of the interpolation regions\n"
+    "    interpolants   the interpolation types of the interpolation regions,\n"
+    "                   see InterpolationType for all interpolation types"
+  )
+  .def(
+
+    python::init< std::vector< double >,
+                  std::vector< double >,
+                  InterpolationType >(),
+    python::arg( "energies" ), python::arg( "values" ),
+    python::arg( "interpolant" ) = InterpolationType::LinearLinear,
+    "Initialise the level spacing table\n\n"
+    "Arguments:\n"
+    "    self           the level spacing table\n"
+    "    energies       the energy values\n"
+    "    values         the level spacing values\n"
+    "    interpolant    the interpolation type (default lin-lin),\n"
+    "                   see InterpolationType for all interpolation types"
+  )
+  .def_property_readonly(
+
+    "energies",
+    &Component::energies,
+    "The energy values"
+  )
+  .def_property_readonly(
+
+    "values",
+    &Component::values,
+    "The level spacing values"
+  )
+  .def_property_readonly(
+
+    "lower_energy_limit",
+    &Component::lowerEnergyLimit
+    "The lower energy limit"
+  )
+  .def_property_readonly(
+
+    "upper_energy_limit",
+    &Component::upperEnergyLimit
+    "The upper energy limit"
+  )
+  .def(
+
+    "__call__",
+    [] ( const Component& self, double energy ) -> decltype(auto)
+       { return self( energy ); },
+    python::arg( "energy" ),
+    "Evaluate the table for a given energy value\n\n"
+    "Arguments:\n"
+    "    self      the table\n"
+    "    energy    the energy value"
+  );
+
+  // add standard equality comparison definitions
+  addStandardEqualityComparisonDefinitions< Component >( component );
+
+  // add standard tabulated data definitions
+  addStandardTabulatedDefinitions< Component >( component );
+
+  // add standard copy definitions
+  addStandardCopyDefinitions< Component >( component );
+}
+
+} // resonances namespace
+} // dryad namespace
