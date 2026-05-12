81f69e278d58f98ae0f57304667ab6796a182938
Alec William Golas <awgolas@pn2609880.lanl.gov>
2026-05-12 16:24:06 -0600

    added more primitive constructor delegation

--- src/njoy/dryad/resonances/UnresolvedResonanceTable.hpp
+++ src/njoy/dryad/resonances/UnresolvedResonanceTable.hpp
@@ -2,7 +2,12 @@
 #define NJOY_DRYAD_RESONANCES__UNRESOLVEDRESONANCETABLE
 
 #include <vector>
+#include <algorithm>
+#include <numeric>
+#include <tuple>
+#include <optional>
 
+#include "tools/apply_permutation.hpp"
 #include "njoy/dryad/resonances/TabulatedLevelSpacing.hpp"
 #include "njoy/dryad/resonances/TabulatedAverageWidths.hpp"
 #include "njoy/dryad/id/ChannelID.hpp"
@@ -28,15 +33,17 @@ namespace resonances{
       std::vector< TabulatedAverageWidths > widths_;
       TabulatedLevelSpacing spacings_;
 
+       /* auxiliary functions */
+       #include "njoy/dryad/resonances/UnresolvedResonanceTable/src/processTable.hpp"
 
-      /* methods */
-    }
 
     
 
     public:
       /* constructor */
       #include "njoy/dryad/resonances/UnresolvedResonanceTable/src/ctor.hpp"
+
+      /* methods */
 
       /**
        * @brief return the channel identifiers
@@ -53,6 +60,33 @@ namespace resonances{
         return this->channels_;
       }
 
+      /**
+       * @brief Return the average widths
+       */
+      const std::vector< TabulatedAverageWidths >& widths() const {
+        return this->widths_;
+      }
+
+      /**
+       * @brief Return the average widths
+       */
+      std::vector< TabulatedAverageWidths >& widths() {
+        return this->widths_;
+      }
+
+      /**
+       * @brief Return the level spacings
+       */
+      const TabulatedLevelSpacing& spacings() const {
+        return this->spacings_;
+      }
+
+      /**
+       * @brief Return the level spacings
+       */
+      TabulatedLevelSpacing& spacings() {
+        return this->spacings_;
+      }
 
 
 } // namespace resonances
--- src/njoy/dryad/resonances/UnresolvedResonanceTable/src/ctor.hpp
+++ src/njoy/dryad/resonances/UnresolvedResonanceTable/src/ctor.hpp
@@ -3,13 +3,18 @@ private:
   /**
    * @brief Private constructor of UnresolvedResonanceTable
    * 
+   * @param[in] channels     the channel identifiers (nc values)
+   * @param[in] widths       TabulatedAverageWidths (nc arrays)
+   * @param[in] spacings     TabulatedLevelSpacing
    */
   UnresolvedResonanceTable( std::vector< id::ChannelID > channels,
                             std::vector< TabulatedAverageWidths > widths,
                             TabulatedLevelSpacing spacings ) :
     channels_( std::move( channels ) ),
     widths_( std::move( widths ) ),
-    spacings_( std::move( spacings ) ) {}
+    spacings_( std::move( spacings ) ) {
+      processTable( this->channels_, this->widths_ );
+    }
 
 public:
 
@@ -23,3 +28,22 @@ public:
 
   UnresolvedResonanceTable& operator=( const UnresolvedResonanceTable& ) = default;
   UnresolvedResonanceTable& operator=( UnresolvedResonanceTable&& ) = default;
+
+  /**
+   * @brief Construct level spacing and average widths tables from a common energy grid
+   *
+   * @param[in] channels        the channel identifiers (nc values)
+   * @param[in] energies        the shared energy grid (ne values)
+   * @param[in] spacingValues   level spacings on the energy grid (ne values)
+   * @param[in] widthValues     average widths on the energy grid (nc x ne values)
+   * @param[in] dofs            optional degrees of freedom per channel
+   */
+  UnresolvedResonanceTable( std::vector< id::ChannelID > channels,
+                            std::vector< double > energies,
+                            std::vector< double > spacingValues,
+                            std::vector< std::vector< double > > widthValues,
+                            const std::vector< std::optional< int > >& dofs = {} ) :
+    UnresolvedResonanceTable( channels,
+                            buildWidths( channels, energies, widthValues, dofs ),
+                            TabulatedLevelSpacing( energies, spacingValues ) ) {}
+                                           
