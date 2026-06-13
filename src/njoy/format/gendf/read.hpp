746ead8f8c6d89065f9393cf329fd2d2eafea0ff
Wim Haeck <whaeck@gmail.com>
2026-06-13 11:19:11 -0600

    updating include files

--- src/njoy/format/gendf/read.hpp
+++ src/njoy/format/gendf/read.hpp
@@ -1,3 +1,3 @@
-#include "njoy/format/gendf/covariance/createCrossSectionCovarianceData.hpp"
-#include "njoy/format/gendf/covariance/createCrossSectionCovarianceDataFromFile.hpp"
-#include "njoy/format/gendf/covariance/createCrossSectionCovarianceMatrix.hpp"
+#include "njoy/format/gendf/read/covariance/createCrossSectionCovarianceData.hpp"
+#include "njoy/format/gendf/read/covariance/createCrossSectionCovarianceDataFromFile.hpp"
+#include "njoy/format/gendf/read/covariance/createCrossSectionCovarianceMatrix.hpp"
