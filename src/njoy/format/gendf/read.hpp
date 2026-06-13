f3299a13fb796bf19e63370366b70c52de27ffb8
Wim Haeck <whaeck@gmail.com>
2026-06-13 04:59:53 -0600

    adding include files for gendf

---
+++ src/njoy/format/gendf/read.hpp
@@ -0,0 +1,3 @@
+#include "njoy/format/gendf/covariance/createCrossSectionCovarianceData.hpp"
+#include "njoy/format/gendf/covariance/createCrossSectionCovarianceDataFromFile.hpp"
+#include "njoy/format/gendf/covariance/createCrossSectionCovarianceMatrix.hpp"
