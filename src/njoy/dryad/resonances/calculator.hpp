1202df5ab324c4deb545a9d42281b707c9168ed1
Wim Haeck <whaeck@gmail.com>
2026-06-17 21:09:42 -0600

    updating include files

--- src/njoy/dryad/resonances/calculator.hpp
+++ src/njoy/dryad/resonances/calculator.hpp
@@ -1,2 +1,5 @@
+#include "njoy/dryad/resonances/calculator/Constant.hpp"
+#include "njoy/dryad/resonances/calculator/ShiftFactor.hpp"
+
 #include "njoy/dryad/resonances/calculator/GeneralRMatrix.hpp"
 #include "njoy/dryad/resonances/calculator/ReichMoore.hpp"
