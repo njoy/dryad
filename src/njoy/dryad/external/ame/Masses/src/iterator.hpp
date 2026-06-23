190db6d888d5bf1937fe1af24b537ad088dfb713
Wim Haeck <whaeck@gmail.com>
2026-06-23 15:00:01 -0600

    makign a few changes following review of feature/endf-particle-database-part2

--- src/njoy/dryad/external/ame/Masses/src/iterator.hpp
+++ src/njoy/dryad/external/ame/Masses/src/iterator.hpp
@@ -1,5 +1,8 @@
 static auto iterator( const id::ParticleID& id ) {
 
+  // the id can be for an excited state while the ame data is for
+  // ground states only.
+
   auto iter = Masses::masses_.find( id.groundState() );
   if ( iter == Masses::masses_.end() ) {
 
