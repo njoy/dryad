24778400e37103c5132ab592fbda27d33dd4f5b4
Wim Haeck <whaeck@gmail.com>
2026-06-25 20:43:16 -0600

    removing more include files

--- src/njoy/dryad/ProjectileTarget/src/iterator.hpp
+++
@@ -1,6 +0,0 @@
-auto iterator( const id::ReactionID& id ) const {
-
-  return std::find_if( this->reactions().begin(), this->reactions().end(),
-                       [&id] ( auto&& reaction )
-                             { return reaction.identifier() == id; } );
-}
--- src/njoy/dryad/Reaction.hpp
+++ src/njoy/dryad/Reaction.hpp
@@ -37,12 +37,132 @@ namespace dryad {
 
     /* auxiliary functions */
 
-    #include "njoy/dryad/Reaction/src/iterator.hpp"
+    template < typename Functor >
+    auto iterator( Functor&& functor, std::size_t index ) const {
+
+      auto iter = std::find_if( this->products().begin(), this->products().end(), functor );
+      std::size_t current = index;
+      while ( current != 0 && iter != this->products().end() ) {
+
+        iter = std::find_if( iter + 1, this->products().end(), functor );
+        --current;
+      }
+
+      return iter;
+    }
+
+    auto iterator( const id::ParticleID& type, std::size_t index = 0 ) const {
+
+      auto functor = [&] ( auto&& product )
+                         { return product.productIdentifier() == type; };
+
+      return this->iterator( functor, index );
+    }
+
+    auto iterator( const id::ParticleID& type, std::size_t chain, std::size_t index ) const {
+
+      auto functor = [&] ( auto&& product )
+                         { return product.productIdentifier() == type &&
+                                  product.chainIndex() == chain; };
+
+      return this->iterator( functor, index );
+    }
+
+    /* constructor */
+
+    /**
+     *  @brief Private constructor
+     */
+    Reaction( id::ReactionID&& id,
+              ReactionCategory&& type,
+              std::optional< std::vector< id::ReactionID > >&& partials,
+              TabulatedCrossSection&& xs,
+              std::vector< ReactionProduct >&& products,
+              std::optional< double >&& mass_q,
+              std::optional< double >&& reaction_q,
+              bool normalise ) :
+        id_( std::move( id ) ), category_( std::move( type ) ),
+        partials_( std::move( partials ) ),
+        mass_difference_qvalue_( mass_q ),
+        reaction_qvalue_( reaction_q ),
+        xs_( std::move( xs ) ),
+        products_( std::move( products ) ) {
+
+      if ( normalise ) {
+
+        this->normalise();
+      }
+    }
 
   public:
 
     /* constructor */
-    #include "njoy/dryad/Reaction/src/ctor.hpp"
+
+    /**
+     *  @brief Default constructor (for pybind11 purposes only)
+     */
+    Reaction() = default;
+
+    Reaction( const Reaction& ) = default;
+    Reaction( Reaction&& ) = default;
+
+    Reaction& operator=( const Reaction& ) = default;
+    Reaction& operator=( Reaction&& ) = default;
+
+    /**
+     *  @brief Constructor for primary reactions
+     *
+     *  @param id           the reaction identifier
+     *  @param xs           the cross section of the reaction
+     *  @param products     the reaction products
+     *  @param mass_q       the mass difference Q value
+     *  @param reaction_q   the reaction Q value
+     *  @param normalise    option to indicate whether or not to normalise
+     *                      all probability data (default: no normalisation)
+     */
+    Reaction( id::ReactionID id,
+              TabulatedCrossSection xs,
+              std::vector< ReactionProduct > products = {},
+              std::optional< double > mass_q = std::nullopt,
+              std::optional< double > reaction_q = std::nullopt,
+              bool normalise = false ) :
+        Reaction( std::move( id ),
+                  ReactionCategory::Primary,
+                  std::nullopt,
+                  std::move( xs ),
+                  std::move( products ),
+                  std::move( mass_q ),
+                  std::move( reaction_q ),
+                  normalise ) {}
+
+    /**
+     *  @brief Constructor for summation reactions
+     *
+     *  Summation reactions do not have Q values associated to them. A cross section
+     *  weighted Q value could be calculated using the partial reactions making up
+     *  the summation reaction.
+     *
+     *  @param id          the reaction identifier
+     *  @param xs          the cross section of the reaction
+     *  @param partials    the identifiers of the partials of the reaction
+     *  @param products    the reaction products associated to the summation reaction
+     *                     (defaults to no reaction products)
+     *  @param normalise   option to indicate whether or not to normalise
+     *                     all probability data (default: no normalisation)
+     */
+    Reaction( id::ReactionID id,
+              std::vector< id::ReactionID > partials,
+              TabulatedCrossSection xs,
+              std::vector< ReactionProduct > products = {},
+              bool normalise = false ) :
+        Reaction( std::move( id ),
+                  ReactionCategory::Summation,
+                  std::move( partials ),
+                  std::move( xs ),
+                  std::move( products ),
+                  std::nullopt,
+                  std::nullopt,
+                  normalise ) {}
 
     /* methods */
 
