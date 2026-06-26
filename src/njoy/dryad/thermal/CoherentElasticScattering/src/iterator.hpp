a4aadd8f5f733fb780bbb78ec107bdab4a547c3b
Wim Haeck <whaeck@gmail.com>
2026-06-25 21:26:20 -0600

    updating the thermal scattering classes

--- src/njoy/dryad/thermal/CoherentElasticScattering/src/iterator.hpp
+++
@@ -1,7 +0,0 @@
-auto iterator( double temperature ) const {
-
-  return std::lower_bound( this->braggEdges().begin(), this->braggEdges().end(),
-                           temperature,
-                           [] ( auto&& edges, auto&& right )
-                              { return edges.temperature() < right; } );
-}
