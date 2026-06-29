3ad2d79a29cd2c0acc030dd8606ffca116b5e9b0
Wim Haeck <whaeck@gmail.com>
2026-06-29 16:04:49 -0600

    adding cross section calculation for coehrent elastic

--- test/dryad/thermal/CoherentElasticScattering.test.cpp
+++ test/dryad/thermal/CoherentElasticScattering.test.cpp
@@ -151,4 +151,29 @@ void verifyChunk( const CoherentElasticScattering& chunk ) {
   CHECK_THAT( 5.         , WithinRel( edge1.energies()[1] ) );
   CHECK_THAT( 1e-2       , WithinRel( edge1.values()[0] ) );
   CHECK_THAT( 1.         , WithinRel( edge1.values()[1] ) );
+
+  // check cross section generation
+  auto xs = chunk.crossSection( 293.6 );
+
+  CHECK( 4 == xs.numberPoints() );
+  CHECK( 2 == xs.numberRegions() );
+  CHECK( 4 == xs.energies().size() );
+  CHECK( 4 == xs.values().size() );
+  CHECK( 2 == xs.boundaries().size() );
+  CHECK( 2 == xs.interpolants().size() );
+  CHECK( 1 == xs.boundaries()[0] );
+  CHECK( 3 == xs.boundaries()[1] );
+  CHECK( InterpolationType::LogLog == xs.interpolants()[0] );
+  CHECK_THAT( 5.219736e-3, WithinRel( xs.energies()[0] ) );
+  CHECK_THAT( 5.         , WithinRel( xs.energies()[1] ) );
+  CHECK_THAT( 5.         , WithinRel( xs.energies()[2] ) );
+  CHECK_THAT( 10.        , WithinRel( xs.energies()[3] ) );
+  CHECK_THAT( 8.703783e-3 / 5.219736e-3, WithinRel( xs.values()[0] ) );
+  CHECK_THAT( 8.703783e-3 / 5.         , WithinRel( xs.values()[1] ) );
+  CHECK_THAT( 9.484639e-1 / 5.         , WithinRel( xs.values()[2] ) );
+  CHECK_THAT( 9.484639e-1 / 10.        , WithinRel( xs.values()[3] ) );
+  CHECK( false == xs.isLinearised() );
+
+  // check throw on temperature that is too far from the stored ones
+  CHECK_THROWS( chunk.crossSection( 400 ) );
 }
