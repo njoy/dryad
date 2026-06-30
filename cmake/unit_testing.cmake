3ec6cd8687dbff7c3a09be443c043e0b8c3a058b
Wim Haeck <whaeck@gmail.com>
2026-06-30 13:27:33 -0600

    adding the incoherent elastic angular pdf

--- cmake/unit_testing.cmake
+++ cmake/unit_testing.cmake
@@ -98,8 +98,9 @@ add_cpp_test( dryad.thermal.TabulatedScatteringKernel                           
 add_cpp_test( dryad.thermal.BraggEdgeData                                              dryad/thermal/BraggEdgeData.test.cpp )
 add_cpp_test( dryad.thermal.DebyeWallerIntegralData                                    dryad/thermal/DebyeWallerIntegralData.test.cpp )
 add_cpp_test( dryad.thermal.CoherentElasticScattering                                  dryad/thermal/CoherentElasticScattering.test.cpp )
-add_cpp_test( dryad.thermal.IncoherentElasticScattering                                dryad/thermal/IncoherentElasticScattering.test.cpp )
 add_cpp_test( dryad.thermal.IncoherentElasticCrossSection                              dryad/thermal/IncoherentElasticCrossSection.test.cpp )
+add_cpp_test( dryad.thermal.IncoherentElasticAngularPdf                                dryad/thermal/IncoherentElasticAngularPdf.test.cpp )
+add_cpp_test( dryad.thermal.IncoherentElasticScattering                                dryad/thermal/IncoherentElasticScattering.test.cpp )
 
 add_cpp_test( dryad.Documentation                                                      dryad/Documentation.test.cpp )
 
