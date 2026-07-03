2e4682f2259c94e025238beac3a2bc1f48108d89
Wim Haeck <whaeck@gmail.com>
2026-07-03 12:48:47 -0600

    updating scion dependency and implementing the multigroup cross section

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -19,7 +19,7 @@ shacl_FetchContent_Declare( tools
 
 shacl_FetchContent_Declare( scion
     GIT_REPOSITORY  ../../njoy/scion
-    GIT_TAG         65b4d848110bd52b8c8167226501c9312f4b0856  # develop - NOT A RELEASED VERSION - CHANGE ME!
+    GIT_TAG         36a7d0ccf39a5544c6777b263bcda3cde3d2a3c5  # develop - NOT A RELEASED VERSION - CHANGE ME!
     )
 
 shacl_FetchContent_Declare( ACEtk
