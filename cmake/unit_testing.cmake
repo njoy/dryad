3f647794bd57d5010157197cf1df0091746b7844
Wim Haeck <whaeck@gmail.com>
2026-07-27 21:24:26 -0600

    adding a function to create GNDS interpolation types

--- cmake/unit_testing.cmake
+++ cmake/unit_testing.cmake
@@ -399,6 +399,7 @@ add_cpp_test( format.gnds.read.createThermalScattering                          
 add_cpp_test( format.gnds.read.createThermalScatteringFromFile                         format/gnds/read/createThermalScatteringFromFile.test.cpp )
 
 add_cpp_test( format.gnds.write.determineDepth                                         format/gnds/write/determineDepth.test.cpp )
+add_cpp_test( format.gnds.write.createInterpolationType                                format/gnds/write/createInterpolationType.test.cpp )
 add_cpp_test( format.gnds.write.toString                                               format/gnds/write/toString.test.cpp )
 add_cpp_test( format.gnds.write.insertDouble                                           format/gnds/write/insertDouble.test.cpp )
 add_cpp_test( format.gnds.write.insertInteger                                          format/gnds/write/insertInteger.test.cpp )
