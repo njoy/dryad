c534c641d2de7bedf8e73e2ef7358fd51ad5f9a7
Wim Haeck <whaeck@gmail.com>
2026-07-30 11:09:23 -0600

    removing more include files

--- src/njoy/dryad/resonances/TabulatedWaveFunction.hpp
+++ src/njoy/dryad/resonances/TabulatedWaveFunction.hpp
@@ -22,6 +22,16 @@ namespace resonances {
   class TabulatedWaveFunction :
       protected scion::math::InterpolationTable< double, double > {
 
+    /* constructor */
+
+    /**
+     *  @brief Private constructor
+     *
+     *  @param table   the interpolation table
+     */
+    TabulatedWaveFunction( InterpolationTable< double, double > table ) :
+      InterpolationTable( std::move( table ) ) {}
+
   public:
 
     /* type aliases */
@@ -30,7 +40,43 @@ namespace resonances {
 
     /* constructor */
 
-    #include "njoy/dryad/resonances/TabulatedWaveFunction/src/ctor.hpp"
+    /**
+     *  @brief Default constructor (for pybind11 purposes only)
+     */
+    TabulatedWaveFunction() = default;
+
+    TabulatedWaveFunction( const TabulatedWaveFunction& ) = default;
+    TabulatedWaveFunction( TabulatedWaveFunction&& ) = default;
+
+    TabulatedWaveFunction& operator=( const TabulatedWaveFunction& ) = default;
+    TabulatedWaveFunction& operator=( TabulatedWaveFunction&& ) = default;
+
+    /**
+     *  @brief Constructor
+     *
+     *  @param ratios         the ratio values
+     *  @param values         the wave function values
+     *  @param boundaries     the boundaries of the interpolation regions
+     *  @param interpolants   the interpolation types of the interpolation regions
+     */
+    TabulatedWaveFunction( std::vector< double > ratios,
+                           std::vector< double > values,
+                           std::vector< std::size_t > boundaries,
+                           std::vector< InterpolationType > interpolants ) :
+      InterpolationTable( std::move( ratios ), std::move( values ),
+                          std::move( boundaries ), std::move( interpolants ) ) {}
+
+    /**
+     *  @brief Constructor for a wave function using a single interpolation zone
+     *
+     *  @param ratios        the ratio values
+     *  @param values        the wave function values
+     *  @param interpolant   the interpolation type of the data (default lin-lin)
+     */
+    TabulatedWaveFunction( std::vector< double > ratios,
+                           std::vector< double > values,
+                           InterpolationType interpolant = InterpolationType::LinearLinear ) :
+      InterpolationTable( std::move( ratios ), std::move( values ), interpolant ) {}
 
     /* methods */
 
