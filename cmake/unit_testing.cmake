7d228240967d350561b2baf0c28a9b61e4a38653
Wim Haeck <whaeck@gmail.com>
2026-08-06 15:16:09 -0600

    adding a function to convert formalism into a string fro gnds writing

--- cmake/unit_testing.cmake
+++ cmake/unit_testing.cmake
@@ -425,7 +425,8 @@ add_cpp_test( format.gnds.write.insertTable                                     
 
 add_cpp_test( format.gnds.write.resonances.insertScatteringRadius                      format/gnds/write/resonances/insertScatteringRadius.test.cpp )
 add_cpp_test( format.gnds.write.resonances.insertHardSphereRadius                      format/gnds/write/resonances/insertHardSphereRadius.test.cpp )
-add_cpp_test( format.gnds.write.resonances.rmatrix.insertResonanceParameters           format/gnds/write/resonances/insertResonanceParameters.test.cpp )
+add_cpp_test( format.gnds.write.resonances.rmatrix.createFormalism                     format/gnds/write/resonances/rmatrix/createFormalism.test.cpp )
+add_cpp_test( format.gnds.write.resonances.rmatrix.insertResonanceParameters           format/gnds/write/resonances/rmatrix/insertResonanceParameters.test.cpp )
 add_cpp_test( format.gnds.write.insertDocumentation                                    format/gnds/write/insertDocumentation.test.cpp )
 add_cpp_test( format.gnds.write.insertCrossSection                                     format/gnds/write/insertCrossSection.test.cpp )
 add_cpp_test( format.gnds.write.insertMultiplicity                                     format/gnds/write/insertMultiplicity.test.cpp )
