e1be6868f47f066fb90f190f1a325014895d23ab
Wim Haeck <whaeck@gmail.com>
2026-08-06 18:24:06 -0600

    affing function to create boudnary condition string

--- cmake/unit_testing.cmake
+++ cmake/unit_testing.cmake
@@ -428,6 +428,7 @@ add_cpp_test( format.gnds.write.insertTable                                     
 add_cpp_test( format.gnds.write.resonances.insertScatteringRadius                      format/gnds/write/resonances/insertScatteringRadius.test.cpp )
 add_cpp_test( format.gnds.write.resonances.insertHardSphereRadius                      format/gnds/write/resonances/insertHardSphereRadius.test.cpp )
 add_cpp_test( format.gnds.write.resonances.rmatrix.createFormalism                     format/gnds/write/resonances/rmatrix/createFormalism.test.cpp )
+add_cpp_test( format.gnds.write.resonances.rmatrix.createBoundaryCondition             format/gnds/write/resonances/rmatrix/createBoundaryCondition.test.cpp )
 add_cpp_test( format.gnds.write.resonances.rmatrix.insertResonanceParameters           format/gnds/write/resonances/rmatrix/insertResonanceParameters.test.cpp )
 add_cpp_test( format.gnds.write.insertDocumentation                                    format/gnds/write/insertDocumentation.test.cpp )
 add_cpp_test( format.gnds.write.insertCrossSection                                     format/gnds/write/insertCrossSection.test.cpp )
