5241898e414889f0f107c1da4c096d09721bbf67
Wim Haeck <whaeck@gmail.com>
2026-08-14 16:13:41 -0600

    removing included code for atomic data

--- src/njoy/dryad/atomic/NonRadiativeTransitionData.hpp
+++ src/njoy/dryad/atomic/NonRadiativeTransitionData.hpp
@@ -36,7 +36,35 @@ namespace atomic {
 
     /* constructor */
 
-    #include "njoy/dryad/atomic/NonRadiativeTransitionData/src/ctor.hpp"
+    /**
+     *  @brief Default constructor (for pybind11 purposes only)
+     */
+    NonRadiativeTransitionData() = default;
+
+    NonRadiativeTransitionData( const NonRadiativeTransitionData& ) = default;
+    NonRadiativeTransitionData( NonRadiativeTransitionData&& ) = default;
+
+    NonRadiativeTransitionData& operator=( const NonRadiativeTransitionData& ) = default;
+    NonRadiativeTransitionData& operator=( NonRadiativeTransitionData&& ) = default;
+
+    /**
+     *  @brief Constructor
+     *
+     *  @param originating   the identifier of the subshell from which the
+     *                       vacancy filling electron originated
+     *  @param emitting      the identifier of the subshell from which the
+     *                       emitted electron originated
+     *  @param probability   the probability of the transition
+     *  @param energy        the energy of the emitted electron (default: undefined)
+     */
+    NonRadiativeTransitionData( id::ElectronSubshellID originating,
+                                id::ElectronSubshellID emitting,
+                                double probability,
+                                std::optional< double > energy = std::nullopt ) :
+      originating_shell_( std::move( originating ) ),
+      emitting_shell_( std::move( emitting ) ),
+      probability_( std::move( probability ) ),
+      energy_( std::move( energy ) ) {}
 
     /* methods */
 
