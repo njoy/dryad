492c866bd957e823c0fb6cf69fc1e91965fe36e0
Wim Haeck <whaeck@gmail.com>
2026-08-14 16:36:17 -0600

    updating covariances

--- src/njoy/dryad/covariance/AngularDistributionCovarianceMatrix.hpp
+++ src/njoy/dryad/covariance/AngularDistributionCovarianceMatrix.hpp
@@ -39,11 +39,118 @@ namespace covariance {
 
     /* auxiliary functions */
 
+    /* constructor */
+
+    AngularDistributionCovarianceMatrix( Parent&& base, ReferenceFrame frame ) :
+      Parent( std::move( base ) ), frame_( std::move( frame ) ) {}
+
   public:
 
     /* constructor */
 
-    #include "njoy/dryad/covariance/AngularDistributionCovarianceMatrix/src/ctor.hpp"
+    /**
+     *  @brief Default constructor (for pybind11 purposes only)
+     */
+    AngularDistributionCovarianceMatrix() = default;
+
+    AngularDistributionCovarianceMatrix( const AngularDistributionCovarianceMatrix& ) = default;
+    AngularDistributionCovarianceMatrix( AngularDistributionCovarianceMatrix&& ) = default;
+
+    AngularDistributionCovarianceMatrix& operator=( const AngularDistributionCovarianceMatrix& ) = default;
+    AngularDistributionCovarianceMatrix& operator=( AngularDistributionCovarianceMatrix&& ) = default;
+
+    /**
+     *  @brief Constructor for an on-diagonal angular distribution covariance matrix
+     *
+     *  @param[in] frame         the reference frame of the covariance data
+     *  @param[in] metadata      the row and column metadata
+     *  @param[in] covariances   the covariance matrix
+     *  @param[in] relative      the relative covariance flag (default is true)
+     */
+    AngularDistributionCovarianceMatrix( ReferenceFrame frame,
+                                         AngularDistributionMetadata metadata,
+                                         matrix::Matrix< double > covariances,
+                                         bool relative = true) :
+      Parent( std::move( metadata ), std::move( covariances ), relative ),
+      frame_( std::move( frame ) ) {}
+
+    /**
+     *  @brief Constructor for an off-diagonal angular distribution covariance matrix
+     *
+     *  @param[in] frame            the reference frame of the covariance data
+     *  @param[in] rowMetadata      the row metadata
+     *  @param[in] columnMetadata   the column metadata
+     *  @param[in] covariances      the covariance matrix
+     *  @param[in] relative         the relative covariance flag (default is true)
+     */
+    AngularDistributionCovarianceMatrix( ReferenceFrame frame,
+                                         AngularDistributionMetadata rowMetadata,
+                                         AngularDistributionMetadata columnMetadata,
+                                         matrix::Matrix< double > covariances,
+                                         bool relative = true ) :
+      Parent( std::move( rowMetadata ), std::move( columnMetadata ),
+              std::move( covariances ), relative ),
+      frame_( std::move( frame ) ) {}
+
+    /**
+     *  @brief Constructor for an on-diagonal angular distribution correlation matrix
+     *
+     *  @param[in] frame          the reference frame of the covariance data
+     *  @param[in] metadata       the row and column metadata
+     *  @param[in] deviations     the standard deviations
+     *  @param[in] correlations   the correlation matrix
+     *  @param[in] relative       the relative covariance flag (default is true)
+     */
+    AngularDistributionCovarianceMatrix( ReferenceFrame frame,
+                                         AngularDistributionMetadata metadata,
+                                         std::vector< double > deviations,
+                                         matrix::Matrix< double > correlations,
+                                         bool relative = true ) :
+      Parent( std::move( metadata ), std::move( deviations ),
+              std::move( correlations ), relative ),
+      frame_( std::move( frame ) ) {}
+
+    /**
+     *  @brief Constructor for an off-diagonal angular distribution correlation matrix
+     *
+     *  @param[in] frame              the reference frame of the covariance data
+     *  @param[in] rowMetadata        the row metadata
+     *  @param[in] columnMetadata     the column metadata
+     *  @param[in] rowDeviations      the standard deviations to be applied to each row
+     *  @param[in] columnDeviations   the standard deviations to be applied to each column
+     *  @param[in] correlations       the correlation matrix
+     *  @param[in] relative           the relative covariance flag (default is true)
+     */
+    AngularDistributionCovarianceMatrix( ReferenceFrame frame,
+                                         AngularDistributionMetadata rowMetadata,
+                                         AngularDistributionMetadata columnMetadata,
+                                         const std::vector< double >& rowDeviations,
+                                         const std::vector< double >& columnDeviations,
+                                         matrix::Matrix< double > correlations,
+                                         bool relative = true ) :
+      Parent( std::move( rowMetadata ), std::move( columnMetadata ),
+              rowDeviations, columnDeviations,
+              std::move( correlations ), relative ),
+      frame_( std::move( frame ) ) {}
+
+    /**
+     *  @brief Constructor using eigenvalues and eigenvectors for an on-diagonal
+     *         angular distribution covariance matrix
+     *
+     *  @param[in] frame          the reference frame of the covariance data
+     *  @param[in] metadata       the row and column metadata
+     *  @param[in] eigenvalues    the eigenvalues
+     *  @param[in] eigenvectors   the associated eigenvectors
+     *  @param[in] relative       the relative covariance flag
+     */
+    AngularDistributionCovarianceMatrix( ReferenceFrame frame,
+                                         AngularDistributionMetadata metadata,
+                                         std::vector< double > eigenvalues,
+                                         std::vector< matrix::Vector< double > > eigenvectors,
+                                         bool relative = true ) :
+      Parent( std::move( metadata ), std::move( eigenvalues ),
+              std::move( eigenvectors ), relative ),
+      frame_( std::move( frame ) ) {}
 
     /* methods */
 
@@ -82,7 +189,37 @@ namespace covariance {
     using Parent::calculateCorrelations;
     using Parent::calculateEigenvalues;
 
-    #include "njoy/dryad/covariance/AngularDistributionCovarianceMatrix/src/extract.hpp"
+    /**
+     *  @brief Extract a submatrix from the covariance matrix based
+     *         on selected values of the keys
+     */
+    AngularDistributionCovarianceMatrix
+    extract( const std::optional< id::ReactionID >& reaction,
+             const std::optional< std::size_t >& moment,
+             const std::optional< id::EnergyGroup >& group ) const {
+
+      return AngularDistributionCovarianceMatrix(
+               Parent::extract( reaction, moment, group ),
+               this->frame() );
+    }
+
+    /**
+     *  @brief Extract a submatrix from the covariance matrix based
+     *         on selected values of the keys
+     */
+    AngularDistributionCovarianceMatrix
+    extract( const std::optional< id::ReactionID >& rowReaction,
+             const std::optional< std::size_t >& rowMoment,
+             const std::optional< id::EnergyGroup >& rowGroup,
+             const std::optional< id::ReactionID >& colReaction,
+             const std::optional< std::size_t >& colMoment,
+             const std::optional< id::EnergyGroup >& colGroup ) const {
+
+      return AngularDistributionCovarianceMatrix(
+               Parent::extract( rowReaction, rowMoment, rowGroup,
+                              colReaction, colMoment, colGroup ),
+               this->frame() );
+    }
 
     /**
      *  @brief Comparison operator: equal
--- src/njoy/dryad/covariance/base/CovarianceData.hpp
+++ src/njoy/dryad/covariance/base/CovarianceData.hpp
@@ -21,8 +21,8 @@ namespace base {
   /**
    *  @class
    *  @brief A covariance data base class
-   * 
-   *  This base class stores covariance matrices for the principal reaction dimension, and 
+   *
+   *  This base class stores covariance matrices for the principal reaction dimension, and
    *  assumes that every matrix uses only a single reaction value.
    */
   template < typename CovarianceMatrix, typename Derived >
@@ -41,15 +41,144 @@ namespace base {
 
     /* auxiliary functions */
 
-    #include "njoy/dryad/covariance/base/CovarianceData/src/generateCovariances.hpp"
-    #include "njoy/dryad/covariance/base/CovarianceData/src/iterator.hpp"
-    #include "njoy/dryad/covariance/base/CovarianceData/src/compare.hpp"
+    static std::pair< std::vector< id::ReactionID >, std::vector< Covariance > >
+    generateCovariances( std::vector< CovarianceMatrix > submatrices ) {
+
+      // we are assuming the following:
+      //   - all submatrices are for a single ProjectileTarget
+      //   - each submatrix is for a single row and column reaction
+      //   - all submatrices are on diagonal or above the diagonal
+
+      std::vector< id::ReactionID > reactions;
+      std::vector< Covariance > covariances;
+
+      Derived::sort( submatrices );
+
+      auto add_reaction = [] ( std::vector< id::ReactionID >& reactions, const id::ReactionID& id ) {
+
+        auto iter = std::lower_bound( reactions.begin(), reactions.end(), id );
+        if ( iter == reactions.end() || id != *iter ) {
+
+          reactions.insert( iter, id );
+        }
+      };
+
+      auto iter = submatrices.begin();
+      while ( iter != submatrices.end() ) {
+
+        auto row = iter->rowMetadata().reactionIdentifiers().front();
+        auto column = iter->columnMetadata().reactionIdentifiers().front();
+
+        add_reaction( reactions, row );
+        add_reaction( reactions, column );
+
+        auto next = std::upper_bound( iter, submatrices.end(),
+                                      std::tie( row, column ),
+                                      [] ( auto&& left, auto&& right )
+                                         { return left < std::tie( right.rowMetadata().reactionIdentifiers().front(),
+                                                                   right.columnMetadata().reactionIdentifiers().front() ); } );
+
+        auto size = std::distance( iter, next );
+        if ( size > 1 ) {
+
+          std::vector< CovarianceMatrix > entries;
+          entries.reserve( size );
+          std::move( iter, next, std::back_inserter( entries ) );
+
+          covariances.emplace_back( std::move( entries ) );
+        }
+        else {
+
+          covariances.emplace_back( std::move( *iter ) );
+        }
+
+        iter = next;
+      }
+
+      return std::make_pair( std::move( reactions ), std::move( covariances ) );
+    }
+
+    auto iterator( const id::ReactionID& row, const id::ReactionID& column ) const {
+
+      auto compare = [] ( auto&& left, auto&& right ) {
+
+        auto get_tie = tools::overload{
+
+          [] ( const CovarianceMatrix& entry ) {
+
+            return std::tie( entry.rowMetadata().reactionIdentifiers().front(),
+                             entry.columnMetadata().reactionIdentifiers().front() );
+          },
+          [] ( const std::vector< CovarianceMatrix >& entry ) {
+
+            return std::tie( entry.front().rowMetadata().reactionIdentifiers().front(),
+                             entry.front().columnMetadata().reactionIdentifiers().front() );
+          }
+        };
+
+        return std::visit( get_tie, left ) < right;
+      };
+
+      auto iter = std::lower_bound( this->covariances().begin(), this->covariances().end(),
+                                    std::tie( row, column ),
+                                    compare );
+      return iter;
+    }
+
+    static bool
+    compare( const id::ReactionID& row, const id::ReactionID& column,
+             const Covariance& covariance ) {
+
+      auto get_tie = tools::overload{
+
+        [] ( const CovarianceMatrix& entry ) {
+
+          return std::tie( entry.rowMetadata().reactionIdentifiers().front(),
+                           entry.columnMetadata().reactionIdentifiers().front() );
+        },
+        [] ( const std::vector< CovarianceMatrix >& entry ) {
+
+          return std::tie( entry.front().rowMetadata().reactionIdentifiers().front(),
+                           entry.front().columnMetadata().reactionIdentifiers().front() );
+        }
+      };
+
+      return std::tie( row, column ) == std::visit( get_tie, covariance );
+    }
+
+    /* constructor */
+
+    /**
+     *  @brief Private constructor
+     *
+     *  @param matrices   the covariance matrices
+     */
+    CovarianceData( std::pair< std::vector< id::ReactionID >, std::vector< Covariance > > data ) :
+      covariances_( std::move( data.second ) ),
+      reactions_( std::move( data.first ) ) {}
 
   public:
 
     /* constructor */
 
-    #include "njoy/dryad/covariance/base/CovarianceData/src/ctor.hpp"
+    /**
+     *  @brief Default constructor (for pybind11 purposes only)
+     */
+    CovarianceData() = default;
+
+    CovarianceData( const CovarianceData& ) = default;
+    CovarianceData( CovarianceData&& ) = default;
+
+    CovarianceData& operator=( const CovarianceData& ) = default;
+    CovarianceData& operator=( CovarianceData&& ) = default;
+
+    /**
+     *  @brief Constructor
+     *
+     *  @param matrices   the covariance matrices
+     */
+    CovarianceData( std::vector< CovarianceMatrix > matrices ) :
+      CovarianceData( generateCovariances( matrices ) ) {}
 
     /* methods */
 
