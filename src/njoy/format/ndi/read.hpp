640f3e0630d2670f7e691eecd090e6c26e395a16
Wim Haeck <whaeck@gmail.com>
2026-08-27 12:25:18 -0600

    upadting include files

--- src/njoy/format/ndi/read.hpp
+++ src/njoy/format/ndi/read.hpp
@@ -1,1 +1,8 @@
+#include "njoy/format/ndi/read/createProjectileIdentifier.hpp"
+#include "njoy/format/ndi/read/createTargetIdentifier.hpp"
+
 #include "njoy/format/ndi/read/createMultigroupCrossSection.hpp"
+#include "njoy/format/ndi/read/createMultigroupReaction.hpp"
+#include "njoy/format/ndi/read/createMultigroupReactions.hpp"
+#include "njoy/format/ndi/read/createMultigroupProjectileTarget.hpp"
+#include "njoy/format/ndi/read/createMultigroupProjectileTargetFromFile.hpp"
