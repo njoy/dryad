6554728370eaf157c6e4c338fbb52d56401548f7
Wim Haeck <whaeck@gmail.com>
2026-09-03 11:19:16 -0600

    updating the create functions fro mass and energy to include uncertainty

--- test/format/gnds/read/pops/createMass.test.cpp
+++ test/format/gnds/read/pops/createMass.test.cpp
@@ -12,6 +12,8 @@ using Catch::Matchers::WithinRel;
 // convenience typedefs
 using namespace njoy::format;
 
+std::string chunk();
+
 SCENARIO( "createMass" ) {
 
   GIVEN( "GNDS mass node" ) {
@@ -29,8 +31,40 @@ SCENARIO( "createMass" ) {
 
         auto chunk = gnds::read::pops::createMass( mass, "eval" );
 
-        CHECK_THAT( 1.00866491574, WithinRel( chunk.value() ) );
+        CHECK_THAT( 1.00866491574, WithinRel( chunk.value.value() ) );
+        CHECK( std::nullopt == chunk.uncertainty );
+      } // THEN
+    } // WHEN
+  } // GIVEN
+
+  GIVEN( "a GNDS mass node with an uncertainty" ) {
+
+    pugi::xml_document document;
+    document.load_string( chunk().c_str() );
+    pugi::xml_node mass = document.child( "mass" );
+
+    WHEN( "a single mass node is given" ) {
+
+      THEN( "it can be converted" ) {
+
+        auto chunk = gnds::read::pops::createMass( mass, "eval" );
+
+        CHECK_THAT( 1.008, WithinRel( chunk.value.value() ) );
+        CHECK_THAT( 0.001, WithinRel( chunk.uncertainty.value() ) );
       } // THEN
     } // WHEN
   } // GIVEN
 } // SCENARIO
+
+std::string chunk() {
+
+  return "<mass>"
+         "  <double label=\"eval\" value=\"1.008\" unit=\"amu\">"
+         "    <uncertainty>\n"
+         "      <standard>\n"
+         "        <double value=\"0.001\"/>\n"
+         "      </standard>\n"
+         "    </uncertainty>\n"
+         "  </double>\n"
+         "</mass>\n";
+}
